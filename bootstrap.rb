67dacebde0e78e87ce7f5c072f9ef1a96e381e81
Wincent Colaiuta <win@wincent.com>
2011-10-04 20:55:50 -0700

    bootstrap: only worry about dot-files, nothing else
    I am finding that when I move from machine to machine, I can't
    necessarily always choose the location of my projects. For example, if I
    work on a server managed by Puppet, that decision may already be made
    for me.
    
    So pare this down to the necessary minimum.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- bootstrap.rb
+++ bootstrap.rb
@@ -4,25 +4,7 @@ require 'fileutils'
 require 'pathname'
 
 HOME      = Pathname.new ENV['HOME']
-PERSONAL  = HOME + 'personal/unversioned'
-WORK      = HOME + 'work/unversioned'
-DOT_FILES = PERSONAL + 'wincent.git'
-
-def system_or_die *args
-  unless system *args
-    raise RuntimeError, "command #{args.inspect} exited with status #{$?.exitstatus}"
-  end
-end
-
-def create_directories
-  FileUtils.mkdir_p PERSONAL
-end
-
-def clone_repo
-  return if DOT_FILES.exist?
-  system_or_die 'git', 'clone', '--recursive',
-    'git://git.wincent.com/wincent.git', DOT_FILES
-end
+DOT_FILES = Pathname.new File.expand_path(File.dirname(__FILE__))
 
 def dot_files
   @dot_files ||= Dir[DOT_FILES + 'dot-*'].map do |file|
@@ -51,7 +33,5 @@ def link_dot_files
 end
 
 Dir.chdir(HOME) do
-  create_directories
-  clone_repo
   link_dot_files
 end
