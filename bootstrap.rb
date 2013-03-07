17841a44644bca77f834e2d5a4d788331e3d9a41
Wincent Colaiuta <win@wincent.com>
2013-03-07 01:04:39 -0800

    bootstrap: more concise output
    For the old output, run as:
    
      DEBUG=1 ruby bootstrap.rb
    
    Otherwise, enjoy the new, concise output.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- bootstrap.rb
+++ bootstrap.rb
@@ -7,18 +7,26 @@ HOME      = Pathname.new ENV['HOME']
 BACKUPS   = HOME + '.backups'
 DOT_FILES = Pathname.new File.expand_path(File.dirname(__FILE__))
 
-def dot_files
-  Dir[DOT_FILES + 'dot-*'].map do |file|
+def debug(verbose, brief = nil)
+  if ENV['DEBUG']
+    puts verbose
+  elsif brief
+    print brief
+  end
+end
+
+def dot_files(&block)
+  Dir[DOT_FILES + 'dot-*'].each do |file|
     path      = Pathname.new file
     dot_file  = path.basename.to_s.sub(/\Adot-/, '.')
-    puts "Found dot-file #{dot_file}"
-    path.dirname + dot_file
+    debug "Found dot-file #{dot_file}", "#{dot_file}:"
+    yield path.dirname + dot_file
   end
 end
 
 def delete(file_or_directory)
   if file_or_directory.exist?
-    puts "Removing #{file_or_directory}"
+    debug "Removing #{file_or_directory}", ' remove'
     FileUtils.rm_r file_or_directory, :force => true, :secure => true
   end
 end
@@ -28,17 +36,17 @@ def backup(file_or_directory)
     BACKUPS.mkpath unless BACKUPS.exist?
     destination = BACKUPS + file_or_directory.basename
     delete destination
-    puts "Moving #{file_or_directory} to #{destination}"
+    debug "Moving #{file_or_directory} to #{destination}", ' backup'
     FileUtils.mv file_or_directory, destination, :force => true
   end
 end
 
 def link_dot_files
-  dot_files.each do |file_or_directory|
+  dot_files do |file_or_directory|
     link = HOME + file_or_directory.basename
     backup link
     delete link
-    puts "Symlinking #{file_or_directory} as #{link}"
+    debug "Symlinking #{file_or_directory} as #{link}", " link\n"
     FileUtils.ln_s file_or_directory, link
   end
 end
