989d4072cacb93ca1e192231b177019f90bf9118
Greg Hurrell <greg@hurrell.net>
2014-05-22 09:33:11 -0700

    Vim: add update-bundle.sh script
    An 11-line shell script. A little bit of automation without going
    overboard.
    
    Signed-off-by: Greg Hurrell <greg@hurrell.net>

---
+++ update-bundle.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+git submodule foreach git checkout master
+
+cd .vim/bundle/command-t
+git checkout next
+cd -
+
+git submodule foreach git pull --recurse-submodules
+
+# once YouCompleteMe is stable again, will delete this
+# when YouCompleteMe updates, install it with its install.sh script
+cd .vim/bundle/YouCompleteMe
+git checkout a1feade
+git submodule update --init --recursive
+cd -
+
+vim -c "call pathogen#helptags() | quit"
