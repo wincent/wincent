408abb443dbcc297b15e1fdbbc816e8737ea5474
Greg Hurrell <greg@hurrell.net>
2015-11-25 15:17:59 -0800

    Hammerspoon: add window-level event monitoring
    As noted in the comments, you currently have to do this a very verbose
    way, but will eventually be able to rely on the `hs.window.filter`
    module instead. Only not doing so now because of the warning in the
    docs:
    
    > WARNING: EXPERIMENTAL MODULE. DO NOT USE IN PRODUCTION.
    > This module is for testing purposes only. It can undergo breaking API
    > changes at any point and without notice.
    
    Using a debug-level logger for now during development; will turn it back
    down to "info" once I'm done.

--- roles/dotfiles/files/.hammerspoon/init.lua
+++ roles/dotfiles/files/.hammerspoon/init.lua
@@ -1,6 +1,127 @@
 hs.grid.setGrid('12x12') -- allows us to place on quarters, thirds and halves
 hs.window.animationDuration = 0 -- disable animations
 
+local logLevel = 'debug'
+local log = hs.logger.new('wincent', logLevel)
+
+-- Event-handling
+--
+-- This will become a lot easier once `hs.window.filter`
+-- (http://www.hammerspoon.org/docs/hs.window.filter.html) moves out of
+-- "experimental" status, but until then, using a manual approach as
+-- demonstrated at: https://gist.github.com/tmandry/a5b1ab6d6ea012c1e8c5
+
+local globalWatcher = nil
+local watchers = {}
+local events = hs.uielement.watcher
+
+function handleGlobalEvent(name, eventType, app)
+  if eventType == hs.application.watcher.launched then
+    log.df('[event] launched %s', app:bundleID())
+    watchApp(app)
+  elseif eventType == hs.application.watcher.terminated then
+    -- Only the PID is set for terminated apps, so can't log bundleID.
+    log.df('[event] terminated PID %d', app:pid())
+    unwatchApp(app)
+  end
+end
+
+function handleAppEvent(element, event)
+  if event == events.windowCreated then
+    log.df('[event] window %s created', element:id())
+    watchWindow(element)
+  else
+    log.wf('unexpected app event %d received', event)
+  end
+end
+
+function handleWindowEvent(window, event, watcher, info)
+  if event == events.elementDestroyed then
+    log.df('[event] window %s destroyed', info.id)
+    watcher:stop()
+    watchers[info.pid].windows[info.id] = nil
+  else
+    log.wf('unexpected window event %d received', event)
+  end
+end
+
+function watchApp(app)
+  local pid = app:pid()
+  if watchers[pid] then
+    log.wf('attempted watch for already-watch PID %d', pid)
+    return
+  end
+
+  -- Watch for new windows.
+  local watcher = app:newWatcher(handleAppEvent)
+  watchers[pid] = {
+    watcher = watcher,
+    windows = {},
+  }
+  watcher:start({events.windowCreated})
+
+  -- Watch already-existing windows.
+  for i, window in pairs(app:allWindows()) do
+    watchWindow(window)
+  end
+end
+
+function unwatchApp(app)
+  local pid = app:pid()
+  local appWatcher = watchers[pid]
+  if not appWatcher then
+    log.wf('attempted unwatch for unknown PID %d', pid)
+    return
+  end
+
+  appWatcher.watcher:stop()
+  for i, watcher in pairs(appWatcher.windows) do
+    watcher:stop()
+  end
+  watchers[pid] = nil
+end
+
+function watchWindow(window)
+  local pid = window:application():pid()
+  local windows = watchers[pid].windows
+  if window:isStandard() then
+    local id = window:id()
+    if not windows[id] then
+      local watcher = window:newWatcher(handleWindowEvent, {
+        id = id,
+        pid = pid,
+      })
+      windows[id] = watcher
+      watcher:start({events.elementDestroyed})
+    end
+  end
+end
+
+function initEventHandling()
+  if globalWatcher then
+    log.w('attempted global watch, but watcher already initialized')
+  end
+
+  -- Watch for application-level events.
+  globalWatcher = hs.application.watcher.new(handleGlobalEvent)
+  globalWatcher:start()
+
+  -- Watch already-running applications.
+  local apps = hs.application.runningApplications()
+  for i = 1, #apps do
+    if apps[i]:bundleID() ~= 'org.hammerspoon.Hammerspoon' then
+      watchApp(apps[i])
+    end
+  end
+end
+
+function tearDownEventHandling()
+  globalWatcher:stop()
+  globalWatcher = nil
+end
+
+initEventHandling()
+
 local lastSeenChain = nil
 local lastSeenWindow = nil
 
