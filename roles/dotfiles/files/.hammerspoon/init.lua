41ff4a631899e6eb3792cf34cc94dd21948f29de
Greg Hurrell <greg@hurrell.net>
2015-11-25 16:16:07 -0800

    hammerspoon: auto-reload on config file changes
    Based on: http://www.hammerspoon.org/go/#fancyreload

--- roles/dotfiles/files/.hammerspoon/init.lua
+++ roles/dotfiles/files/.hammerspoon/init.lua
@@ -222,3 +222,16 @@ hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'down', chain({
   grid.centeredBig,
   grid.centeredSmall,
 }))
+
+-- Auto-reload config on change.
+
+function reloadConfig(files)
+  for _, file in pairs(files) do
+    if file:sub(-4) == '.lua' then
+      hs.reload()
+      return
+    end
+  end
+end
+
+hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
