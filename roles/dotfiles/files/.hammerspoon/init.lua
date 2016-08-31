2e0d57ac6065ae75fd6246acfe80ecd670f84b95
Greg Hurrell <greg@hurrell.net>
2016-08-31 00:32:51 -0700

    Hammerspoon: fix console noise that started with v0.9.47
    Been getting all manner of console spam since the upgrade a couple of days ago.
    Many messages like:
    
      ERROR:   LuaSkin: hs.application.watcher callback error:
      ....app/Contents/Resources/extensions/hs/uielement/init.lua:187:
      bad argument #-2 to '_stop'
      (hs.uielement.watcher.userdata expected, got hs.uielement.watcher.userdata)
    
    Nothing suspicious in the release notes. Switching to (the still "experimental")
    `hs.window.filter` API allows me to avoid the code paths that were throwing off
    the errors/warnings, and delete a lot of code to boot. Seems to work well. Yay.

--- roles/dotfiles/files/.hammerspoon/init.lua
+++ roles/dotfiles/files/.hammerspoon/init.lua
@@ -176,52 +176,22 @@ function activateLayout(forceScreenCount)
   layoutConfig._after_()
 end
 
+--
 -- Event-handling
 --
--- This will become a lot easier once `hs.window.filter`
--- (http://www.hammerspoon.org/docs/hs.window.filter.html) moves out of
--- "experimental" status, but until then, using a manual approach as
--- demonstrated at: https://gist.github.com/tmandry/a5b1ab6d6ea012c1e8c5
 
-local globalWatcher = nil
-local watchers = {}
-local events = hs.uielement.watcher
-
-function handleGlobalEvent(name, eventType, app)
-  if eventType == hs.application.watcher.launched then
-    log.df('[event] launched %s', app:bundleID())
-    watchApp(app)
-  elseif eventType == hs.application.watcher.terminated then
-    -- Only the PID is set for terminated apps, so can't log bundleID.
-    local pid = app:pid()
-    log.df('[event] terminated PID %d', pid)
-    unwatchApp(pid)
-  end
-end
-
-function handleAppEvent(element, event)
-  if event == events.windowCreated then
-    if pcall(function()
-      log.df('[event] window %s created', element:id())
-    end) then
-      watchWindow(element)
-    else
-      log.wf('error thrown trying to access element in handleAppEvent')
+function handleWindowEvent(window)
+  if canManageWindow(window) then
+    local application = window:application()
+    local bundleID = application:bundleID()
+    if layoutConfig[bundleID] then
+      layoutConfig[bundleID](window)
     end
-  else
-    log.wf('unexpected app event %d received', event)
   end
 end
 
-function handleWindowEvent(window, event, watcher, info)
-  if event == events.elementDestroyed then
-    log.df('[event] window %s destroyed', info.id)
-    watcher:stop()
-    watchers[info.pid].windows[info.id] = nil
-  else
-    log.wf('unexpected window event %d received', event)
-  end
-end
+local windowFilter=hs.window.filter.new()
+windowFilter:subscribe(hs.window.filter.windowCreated, handleWindowEvent)
 
 function handleScreenEvent()
   -- Make sure that something noteworthy (display count) actually
@@ -234,96 +204,14 @@ function handleScreenEvent()
   end
 end
 
-function watchApp(app)
-  local pid = app:pid()
-  if watchers[pid] then
-    log.wf('attempted watch for already-watched PID %d', pid)
-    return
-  end
-
-  -- Watch for new windows.
-  local watcher = app:newWatcher(handleAppEvent)
-  watchers[pid] = {
-    watcher = watcher,
-    windows = {},
-  }
-  watcher:start({events.windowCreated})
-
-  -- Watch already-existing windows.
-  for _, window in pairs(app:allWindows()) do
-    watchWindow(window)
-  end
-end
-
-function unwatchApp(pid)
-  local appWatcher = watchers[pid]
-  if not appWatcher then
-    log.wf('attempted unwatch for unknown PID %d', pid)
-    return
-  end
-
-  appWatcher.watcher:stop()
-  for _, watcher in pairs(appWatcher.windows) do
-    watcher:stop()
-  end
-  watchers[pid] = nil
-end
-
-function watchWindow(window)
-  local application = window:application()
-  local bundleID = application:bundleID()
-  local pid = application:pid()
-  local windows = watchers[pid].windows
-  if canManageWindow(window) then
-    -- Do initial layout-handling.
-    local bundleID = application:bundleID()
-    if layoutConfig[bundleID] then
-      layoutConfig[bundleID](window)
-    end
-
-    -- Watch for window-closed events.
-    local id = window:id()
-    if id then
-      if not windows[id] then
-        local watcher = window:newWatcher(handleWindowEvent, {
-          id = id,
-          pid = pid,
-        })
-        windows[id] = watcher
-        watcher:start({events.elementDestroyed})
-      end
-    end
-  end
-end
-
 function initEventHandling()
-  -- Watch for application-level events.
-  globalWatcher = hs.application.watcher.new(handleGlobalEvent)
-  globalWatcher:start()
-
-  -- Watch already-running applications.
-  local apps = hs.application.runningApplications()
-  for _, app in pairs(apps) do
-    if app:bundleID() ~= 'org.hammerspoon.Hammerspoon' then
-      watchApp(app)
-    end
-  end
-
-  -- Watch for screen changes.
   screenWatcher = hs.screen.watcher.new(handleScreenEvent)
   screenWatcher:start()
 end
 
 function tearDownEventHandling()
-  globalWatcher:stop()
-  globalWatcher = nil
-
   screenWatcher:stop()
   screenWatcher = nil
-
-  for pid, _ in pairs(watchers) do
-    unwatchApp(pid)
-  end
 end
 
 initEventHandling()
