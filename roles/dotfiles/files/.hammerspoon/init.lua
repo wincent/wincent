dc79cda0c0e6c5531b1b54ce600300ab2f5793da
Greg Hurrell <greg@hurrell.net>
2016-11-17 19:08:45 -0800

    hammerspoon: move table.soString into separate util module
    Now it is just "t":
    
    ```
    local util = require 'util'
    util.t({sample = true })
    
    -- Or more tersely:
    local t = util.t
    t({sample = true})
    ```

--- roles/dotfiles/files/.hammerspoon/init.lua
+++ roles/dotfiles/files/.hammerspoon/init.lua
@@ -376,46 +376,6 @@ end)
 -- `open hammerspoon://screencast`
 hs.urlevent.bind('screencast', prepareScreencast)
 
---
--- General utility
---
-
-function table.val_to_str ( v )
-  if "string" == type( v ) then
-    v = string.gsub( v, "\n", "\\n" )
-    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
-      return "'" .. v .. "'"
-    end
-    return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
-  else
-    return "table" == type( v ) and table.tostring( v ) or
-      tostring( v )
-  end
-end
-
-function table.key_to_str ( k )
-  if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
-    return k
-  else
-    return "[" .. table.val_to_str( k ) .. "]"
-  end
-end
-
-function table.tostring( tbl )
-  local result, done = {}, {}
-  for k, v in ipairs( tbl ) do
-    table.insert( result, table.val_to_str( v ) )
-    done[ k ] = true
-  end
-  for k, v in pairs( tbl ) do
-    if not done[ k ] then
-      table.insert( result,
-        table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
-    end
-  end
-  return "{" .. table.concat( result, "," ) .. "}"
-end
-
 eventtap.init()
 reloader.init()
 
---
+++ roles/dotfiles/files/.hammerspoon/util.lua
@@ -0,0 +1,48 @@
+--
+-- General utility
+--
+
+-- Forward function declarations.
+local valueToString = nil
+local keyToString = nil
+local tableToString = nil
+
+valueToString = (function(v)
+  if "string" == type( v ) then
+    v = string.gsub( v, '\n', '\\n' )
+    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+      return "'" .. v .. "'"
+    end
+    return '"' .. string.gsub(v, '"', '\\"' ) .. '"'
+  else
+    return 'table' == type( v ) and tableTostring( v ) or
+      tostring( v )
+  end
+end)
+
+keyToString = (function(k)
+  if 'string' == type( k ) and string.match( k, '^[_%a][_%a%d]*$' ) then
+    return k
+  else
+    return '[' .. valueToString(k) .. ']'
+  end
+end)
+
+tableToString = (function(tbl)
+  local result, done = {}, {}
+  for k, v in ipairs( tbl ) do
+    table.insert(result, valueToString(v))
+    done[ k ] = true
+  end
+  for k, v in pairs( tbl ) do
+    if not done[ k ] then
+      table.insert(
+        result,
+        keyToString(k) .. '=' .. valueToString(v )
+      )
+    end
+  end
+  return '{' .. table.concat( result, ',' ) .. '}'
+end)
+
+return {t = tableToString}
