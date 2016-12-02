de6c3b118825087f130f3e787decaa2b95407a1a
Greg Hurrell <greg@hurrell.net>
2016-12-01 17:13:08 -0800

    mutt: configure offlineimap
    Based on:
    
    http://stevelosh.com/blog/2012/10/the-homely-mutt/
    
    Relies on the password being store in the login keychain with:
    
    - item name: http://imap.gmail.com
    - account name: greg+mutt@hurrell.net
    
    Note that when we call the Python function to retrieve the password we
    pass in the "server" matching the "item name", but without the scheme:
    
    - account: greg+mutt@hurrell.net
    - server: imap.gmail.com

---
+++ roles/dotfiles/files/.mutt/offlineimap.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import re, subprocess
+
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command': 'find-internet-password',
+        'account': account,
+        'server': server,
+        'keychain': '/Users/glh/Library/Keychains/login.keychain',
+    }
+    command = "%(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    outtext = [l for l in output.splitlines()
+               if l.startswith('password: ')][0]
+    return re.match(r'password: "(.*)"', outtext).group(1)
