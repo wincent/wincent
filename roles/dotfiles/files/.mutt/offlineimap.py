6d1cbd80c26be59c488f7916f288e82bbce4f0a1
Greg Hurrell <greg@hurrell.net>
2016-12-05 01:02:42 -0800

    mutt: embed blacklist as YAML in config file
    Yes, this is crazy, but editing that Python expression in a way that
    doesn't violate whitespace constraints is just too hard and annoying. So
    now we have a YAML string in a Python expression in a config file. Yay.

--- roles/dotfiles/files/.mutt/offlineimap.py
+++ roles/dotfiles/files/.mutt/offlineimap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os, subprocess
+import os, subprocess, yaml
 
 def get_keychain_pass(account=None, server=None):
     params = {
@@ -11,3 +11,7 @@ def get_keychain_pass(account=None, server=None):
     command = "%(command)s %(account)s %(server)s" % params
     output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
     return output.strip()
+
+def copy_ignore(text):
+    blacklist = yaml.load(text)
+    return lambda foldername: blacklist.get(foldername)
