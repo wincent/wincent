269327ee552a8aaadf15bae117bccc59f11fca13
Greg Hurrell <greg@hurrell.net>
2016-12-07 10:25:19 -0800

    mutt: partial refactor of filtering
    Confession: haven't even run this, but I wanted to show it as an
    intermediate step. The idea is for each rule, start selecting from
    scratch (caching should make this fast), so as to avoid issues with
    latter rules operating on messages that have already been archived.
    
    I want to change the "DSL" though, so will do that in the next commit
    and actually test the darn thing.

--- roles/dotfiles/files/.imapfilter/work.lua
+++ roles/dotfiles/files/.imapfilter/work.lua
@@ -63,98 +63,151 @@ function run()
   work = connect()
   inbox = work.INBOX
 
-  archived = Set {}
+  --
+  -- Actions
+  --
 
   archive = (function(messages, description)
-    messages = messages - archived
     print_status(messages, description .. ' -> archive')
     messages:move_messages(work.Archive)
-    archived = archived + messages
   end)
 
   archive_and_mark_read = (function(messages, description)
-    messages = messages - archived
     print_status(messages, description .. ' -> archive & mark read')
     messages:mark_seen()
     messages:move_messages(work.Archive)
-    archived = archived + messages
   end)
 
   --
-  -- Phabricator
+  -- Queries
   --
 
-  phabricator = inbox:match_field('X-Phabricator-Sent-This-Message', '.')
-  differential = phabricator:contain_subject('[Differential]')
-  reviewer = differential:match_field('X-Differential-Reviewer', phabricator_user)
-  commented = differential:match_field('X-Phabricator-Mail-Tags', '<differential-comment>')
-  uncommented = differential - commented
-  mine = differential:match_field('X-Differential-Author', phabricator_user)
+  get = {
+    -- All Differential notifications.
+    differential = (function()
+      return inbox:
+        contain_subject('[Differential]'):
+        match_field('X-Phabricator-Sent-This-Message', '.')
+    end),
+
+    -- Commented on (by anybody).
+    commented = (function(messages)
+      return messages:match_field(
+        'X-Phabricator-Mail-Tags',
+        '<differential-comment>'
+      )
+    end),
+
+    committed = (function(messages)
+      return messages:match_field(
+        'X-Phabricator-Mail-Tags',
+        '<differential-committed>'
+      )
+    end),
+
+    requested = (function(messages)
+      return messages:match_field(
+        'X-Phabricator-Mail-Tags',
+        '<differential-review-request>'
+      )
+    end),
+
+    planned = (function(messages)
+      return messages:contain_subject('[Planned Changes To]')
+    end),
+
+    -- Authored (by me).
+    authored = (function(messages)
+      return messages:match_field('X-Differential-Author', phabricator_user)
+    end),
+
+    -- Me as a reviewer.
+    reviewer = (function(messages)
+      return messages:match_field('X-Differential-Reviewer', phabricator_user)
+    end),
+  }
+
+  --
+  -- Utility
+  --
+
+  -- Phabricator puts a `In-Reply-To` header containing an `Message-ID` of the
+  -- original request.
+  --
+  -- Pass options `{all = true}` to select all matching messages. By default
+  -- (`{all = false}`), only messages older than `messages` are considered.
+  differential_related = (function(messages, options)
+    all = options and options.all
+    related = Set {}
+    for _, message in ipairs(messages) do
+      mbox, uid = table.unpack(message)
+      m = mbox[uid]
+      parent_date = all or parse_internal_date(m:fetch_date())
+      revision_id = string.gsub(
+        mbox[uid]:fetch_field('In-Reply-To'),
+        'In%-Reply%-To: ',
+        ''
+      )
+      related = differential:match_field('In-Reply-To', revision_id) +
+        differential:match_field('Message-ID', revision_id)
+      for _, message in ipairs(related) do
+        mbox, uid = table.unpack(message)
+        m = mbox[uid]
+        date = all or parse_internal_date(m:fetch_date())
+        if all or date <= parent_date then
+          table.insert(related, message)
+        end
+      end
+    end
+    return related
+  end)
+
+  --
+  -- Differential
+  --
 
   -- Assume that anything I have taken action on, I have seen all previous
   -- actions.
-  --
-  -- Phabricator puts a `Reply-To` header containing an ID which includes the
-  -- diff number, and all subsequent notifications reference that ID in their
-  -- `In-Reply-To` header.
-  messages = differential:contain_from(me)
-  touched = Set {}
-  for _, message in ipairs(messages) do
-    mbox, uid = table.unpack(message)
-    m = mbox[uid]
-    parent_date = parse_internal_date(m:fetch_date())
-    revision_id = string.gsub(mbox[uid]:fetch_field('In-Reply-To'), 'In%-Reply%-To: ', '')
-    related = differential:match_field('In-Reply-To', revision_id) + differential:match_field('Message-ID', revision_id)
-    for _, message in ipairs(related) do
-      mbox, uid = table.unpack(message)
-      m = mbox[uid]
-      date = parse_internal_date(m:fetch_date())
-      if date <= parent_date then
-        table.insert(touched, message)
-      end
-    end
-  end
-  messages = messages + touched
+  differential = get.differential():contain_from(me)
+  messages = differential + differential_related(differential)
   archive_and_mark_read(messages, 'My [Phabricator] actions')
 
-  closed = differential:contain_subject('[Closed]')
-  messages = closed:match_field('X-Phabricator-Mail-Tags', '<differential-committed>')
+  messages = get.differential():contain_subject('[Closed]')
+  messages = get.committed(messages)
   archive(messages, '[Closed]')
 
-  accepted_and_shipped = differential:contain_subject('[Accepted and Shipped]')
-  messages = accepted_and_shipped * uncommented
-  archive(messages, '[Accepted and Shipped] without comments')
+  accepted_and_shipped = get.differential():
+    contain_subject('[Accepted and Shipped]')
+  commented = get.commented(accepted_and_shipped)
+  uncommented = accepted_and_shipped - commented
+  archive(uncommented, '[Accepted and Shipped] without comments')
 
-  accepted = differential:contain_subject('[Accepted]')
-  interim = accepted * uncommented
-  messages = (accepted * uncommented) - mine
+  accepted = get.differential():contain_subject('[Accepted]')
+  commented = get.commented(accepted)
+  uncommented = accepted - commented
+  messages = uncommented - get.authored(uncommented)
   archive(messages, "[Accepted] without comments (others' diffs)")
 
   -- Metadata changes (not "[Updated, N line(s)]") without comments.
-  updated = differential:contain_subject('[Updated]')
-  messages = updated - commented
+  updated = get.differential():contain_subject('[Updated]')
+  messages = updated - get.commented(updated)
   archive(messages, '[Updated] without comments')
 
-  planned = differential:contain_subject('[Planned Changes To]')
-  messages = planned - commented
+  planned = get.planned(get.differential())
+  messages = planned - get.commented(planned)
   archive(messages, '[Planned Changes To] without comments')
 
   -- If I'm not direct reviewer, I can probably ignore these.
-  messages = planned - reviewer
+  planned = get.planned(get.differential())
+  messages = planned - get.reviewer(planned)
   archive(messages, '[Planned Changes To] not direct reviewer')
 
   -- trunkagent's comments on other people's diffs.
-  messages = differential:contain_from('trunkagent') - mine
+  messages = get.differential():contain_from('trunkagent')
+  messages = messages - get.authored(messages)
   archive(messages, "trunkagent comments on other people's diffs")
 
-  -- 'Ch1rpBot' matches from, but 'Ch1rpBot <noreply@fb.com>' does not.
-  chirp_bot = inbox:contain_from('Ch1rpBot')
-  messages = chirp_bot:contain_subject('[land] [success]')
-  archive(messages, '[land] [success]')
-
-  requests = differential:
-    match_field('X-Phabricator-Mail-Tags', '<differential-review-request'):
-    is_unflagged()
+  requests = get.requested(get.differential()):is_unflagged()
   self = requests:match_field('X-Differential-Reviewer', phabricator_user)
   team = requests:match_field('X-Differential-Reviewer', phabricator_team)
   messages = requests * (self + team)
@@ -162,17 +215,27 @@ function run()
   messages:mark_flagged()
 
   -- Archive abandoned and related emails as well.
-  abandoned = differential:contain_subject('[Abandoned]'):
+  abandoned = get.differential():contain_subject('[Abandoned]'):
     match_field('X-Differential-Status', 'Abandoned')
-  for _, message in ipairs(abandoned) do
-    mbox, uid = table.unpack(message)
-    revision_id = string.gsub(mbox[uid]:fetch_field('In-Reply-To'), 'In%-Reply%-To: ', '')
-    related = differential:match_field('In-Reply-To', revision_id) + differential:match_field('Message-ID', revision_id)
-    abandoned = abandoned + related
-  end
-  archive(abandoned, '[Abandoned] + related')
+  messages = abandoned + differential_related(abandoned, {all = true})
+  archive(messages, '[Abandoned] + related')
 
-  messages = inbox:contain_from('facebookmail.com'):match_field('X-Facebook-Notify', 'page_fan')
+  --
+  -- Miscellaneous
+  --
+
+  -- 'Ch1rpBot' matches from, but 'Ch1rpBot <noreply@fb.com>' does not.
+  chirp_bot = inbox:contain_from('Ch1rpBot')
+  messages = chirp_bot:contain_subject('[land] [success]')
+  archive(messages, '[land] [success]')
+
+  --
+  -- Business
+  --
+
+  messages = inbox:
+    contain_from('facebookmail.com'):
+    match_field('X-Facebook-Notify', 'page_fan')
   archive(messages, 'Page notifications')
 end
 
