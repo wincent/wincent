2536dd9d91a3074dcfa73645551a78b06cddf9b0
Greg Hurrell <greg@hurrell.net>
2016-12-06 23:18:58 -0800

    mutt: use my Phabricator actions as a cue to prune older notifications
    We assume that if I was on a diff commenting on it, I read all the
    content that had been previously published to it, so we can mark it as
    read and archive.

--- roles/dotfiles/files/.imapfilter/work.lua
+++ roles/dotfiles/files/.imapfilter/work.lua
@@ -5,6 +5,21 @@ local password = get_pass(me, 'outlook.office365.com')
 local phabricator_user = '<PHID-USER-dfiqtsjr7q4b4fu336uy>'
 local phabricator_team = '<PHID-PROJ-vgzmhfup375n4lfv4xka>'
 
+local MONTHS = {
+  Jan = 1,
+  Feb = 2,
+  Mar = 3,
+  Apr = 4,
+  May = 5,
+  Jun = 6,
+  Jul = 7,
+  Aug = 8,
+  Sep = 9,
+  Oct = 10,
+  Nov = 11,
+  Dec = 12,
+}
+
 function connect()
   return IMAP {
     server = 'outlook.office365.com',
@@ -15,18 +30,44 @@ function connect()
   }
 end
 
+-- Parses an IMAP INTERNALDATE string (RFC 3501).
+--
+-- Expects a string with format "dd-Mon-yyyy hh:mm:ss +hhmm".
+--
+-- See: http://tools.ietf.org/html/rfc3501#section-2.3.3
+function parse_internal_date(date_string)
+  -- Based on: http://stackoverflow.com/a/4600967/2103996
+  format = '(%d+)-(%a+)-(%d+) (%d+):(%d+):(%d+) ([+-]%d+)'
+  day, month, year, hour, min, sec, zone = date_string:match(format)
+  month = MONTHS[month]
+  local_offset = os.time() - os.time(os.date('!*t'))
+  offset = tonumber(zone) - local_offset
+  return os.time({
+    day = day,
+    month = month,
+    year = year,
+    hour = hour,
+    min = min,
+    sec = sec,
+  }) + offset
+end
+
 function run()
   print "work: run"
-
-  work = connect()
-  inbox = work.INBOX
-  archive = work.Archive
 
   -- NOTE: Beware the use of contain_field when talking to an MS server; it is
   -- totally unreliable, so must use the slower match_field method. See:
   --
   -- - https://github.com/lefcha/imapfilter/issues/14
   -- - https://github.com/lefcha/imapfilter/issues/33
+  work = connect()
+  inbox = work.INBOX
+  archive = work.Archive
+
+  --
+  -- Phabricator
+  --
+
   phabricator = inbox:match_field('X-Phabricator-Sent-This-Message', '.')
   differential = phabricator:contain_subject('[Differential]')
   reviewer = differential:match_field('X-Differential-Reviewer', phabricator_user)
@@ -34,7 +75,30 @@ function run()
   uncommented = differential - commented
   mine = differential:match_field('X-Differential-Author', phabricator_user)
 
+  -- Assume that anything I have taken action on, I have seen all previous
+  -- actions.
+  --
+  -- Phabricator puts a `Reply-To` header containing an ID which includes the
+  -- diff number, and all subsequent notifications reference that ID in their
+  -- `In-Reply-To` header.
   messages = phabricator:contain_from(me)
+  touched = Set {}
+  for _, message in ipairs(messages) do
+    mbox, uid = table.unpack(message)
+    m = mbox[uid]
+    parent_date = parse_internal_date(m:fetch_date())
+    revision_id = string.gsub(mbox[uid]:fetch_field('In-Reply-To'), 'In%-Reply%-To: ', '')
+    related = differential:match_field('In-Reply-To', revision_id) + differential:match_field('Reply-To', revision_id)
+    for _, message in ipairs(related) do
+      mbox, uid = table.unpack(message)
+      m = mbox[uid]
+      date = parse_internal_date(m:fetch_date())
+      if date <= parent_date then
+        table.insert(touched, message)
+      end
+    end
+  end
+  messages = messages + touched
   print_status(messages, 'My [Phabricator] actions -> archive & mark read')
   messages:mark_seen()
   messages:move_messages(archive)
@@ -96,8 +160,8 @@ function run()
     match_field('X-Differential-Status', 'Abandoned')
   for _, message in ipairs(abandoned) do
     mbox, uid = table.unpack(message)
-    rev_key = string.gsub(mbox[uid]:fetch_field('In-Reply-To'), 'In%-Reply%-To: ', '')
-    related = differential:match_field('In-Reply-To', rev_key) + differential:match_field('Message-ID', rev_key)
+    revision_id = string.gsub(mbox[uid]:fetch_field('In-Reply-To'), 'In%-Reply%-To: ', '')
+    related = differential:match_field('In-Reply-To', revision_id) + differential:match_field('Reply-To', revision_id)
     abandoned = abandoned + related
   end
   print_status(abandoned, '[Abandoned] + related -> archive')
