da55e4cb7f49004c5b4fdc8dbc3bb26864072419
Greg Hurrell <greg@hurrell.net>
2016-12-08 07:34:51 -0800

    mutt: invoke vim viewer through a wrapper script
    This allows us to do some context-sensitive behavior:
    
    - Outside tmux, just open.
    - Inside tmux, wide window, open in a "vertical" split.
    - Inside tmux, narrow window, open in a "horizontal" split.
    
    (Note that I mean vertical/horizontal in the Vim sense, not the tmux
    sense, as it is more intuitive to me.)

---
+++ roles/dotfiles/files/.mutt/view_mail.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+COLUMNS=${COLUMNS:-`tput cols`}
+TMPFILE="$1"
+ENCODING="$2"
+
+# Need to copy the file because mutt will delete it before Vim can read it.
+DIR=$(mktemp -d)
+cp "$TMPFILE" "$DIR/preview"
+TMPFILE="$DIR/preview"
+
+if [ -z "$TMUX" ]; then
+  view "+set encoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' "$TMPFILE"
+elif [ "$COLUMNS" -gt 180 ]; then
+  tmux split-window -p 50 -h \
+    view "+set encoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' "$TMPFILE"
+else
+  tmux split-window -p 50 -v \
+    view "+set encoding=$ENCODING" '+set filetype=mail' '+set nofoldenable' '+set nomodifiable' "$TMPFILE"
+fi
