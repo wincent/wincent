18e1bc369aaa33a0d539d0a1521d93b9307d72a6
Greg Hurrell <greg@hurrell.net>
2016-12-16 22:05:02 -0800

    mutt: add retry with exponential backoff for sync script
    On failure, will retry after 1, 2, 4, 8 minutes.
    
    I guess this will work; can't really know for sure until I catch a
    failure happening "red-handed".

--- roles/dotfiles/files/.mutt/scripts/sync.sh
+++ roles/dotfiles/files/.mutt/scripts/sync.sh
@@ -6,15 +6,37 @@ if [ $# -ne 1 ]; then
 fi
 
 ACCOUNT="$1"
+BACKOFF=0
+MAX_BACKOFF=480 # 8 minutes
+
+function delay() {
+  if [ $BACKOFF -ne 0 ]; then
+    echo "Backing off for ${BACKOFF}s."
+    sleep $BACKOFF
+  fi
+}
+
+function backoff() {
+  if [ $BACKOFF -eq 0 ]; then
+    BACKOFF=60
+  elif [ $BACKOFF -ge $MAX_BACKOFF ]; then
+    BACKOFF=$MAX_BACKOFF
+  else
+    BACKOFF=$(expr $BACKOFF '*' 2)
+  fi
+}
 
 while true; do
+  delay
+
   echo "Running imapfilter ($ACCOUNT):"
   echo
 
   ONCE=1 time imapfilter -vc "${HOME}/.imapfilter/${ACCOUNT}.lua" || {
     reattach-to-user-namespace \
       terminal-notifier -title imapfilter -message "imapfilter ($ACCOUNT) exited"
-    exit 1
+    backoff
+    continue
   }
 
   echo
@@ -24,7 +46,8 @@ while true; do
   time gtimeout 600 mbsync "$ACCOUNT" || {
     reattach-to-user-namespace \
       terminal-notifier -title mbsync -message "mbsync ($ACCOUNT) exited"
-    exit 1
+    backoff
+    continue
   }
 
   echo
@@ -42,5 +65,6 @@ while true; do
   echo "Sleeping for 1m..."
   echo
 
+  BACKOFF=0
   sleep 60
 done
