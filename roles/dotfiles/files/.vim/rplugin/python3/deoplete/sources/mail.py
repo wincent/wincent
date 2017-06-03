830891847530a4383518c3f9e148413ec8ca4f1d
Greg Hurrell <greg@hurrell.net>
2017-06-02 23:39:59 -0700

    vim: make deoplete port of mail address completion
    I am not a Pythonista, so this was painful to write and is probably
    horrible code, but it works, in a sense.

---
+++ roles/dotfiles/files/.vim/rplugin/python3/deoplete/sources/mail.py
@@ -0,0 +1,67 @@
+from .base import Base
+
+from itertools import chain
+from deoplete.util import parse_buffer_pattern, getlines
+
+import re
+from subprocess import PIPE, Popen
+import string
+
+class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.filetypes = ['mail']
+        self.name = 'mail'
+        self.mark = '[@]'
+        self.min_pattern_length = 0
+        self.limit = 1000000
+
+        self.__pattern = re.compile('^(Bcc|Cc|From|Reply-To|To):(.*, ?| ?)')
+        self.__wrapper = self.__find_reattach_to_user_namespace_binary()
+        self.__binary = self.__find_lbdbq_binary()
+        self.__candidates = None
+
+    def on_event(self, context):
+        self.__candidates = []
+        data = self.__lbdbq('.')
+        if data:
+            for line in data:
+                try:
+                    address, name, source = line.strip().split('\t')
+                    if name:
+                        address = name + ' <' + address + '>'
+                    self.__candidates.append({'word': address, 'kind': source})
+                except:
+                    pass
+
+    def gather_candidates(self, context):
+        result = self.__pattern.search(context['input'])
+        if result is not None:
+            if not self.__candidates:
+                self.on_event(context)
+            return self.__candidates
+
+    def __find_lbdbq_binary(self):
+        return self.vim.call('exepath', 'lbdbq')
+
+    def __find_reattach_to_user_namespace_binary(self):
+        return self.vim.call('exepath', 'reattach-to-user-namespace')
+
+    def __lbdbq(self, query):
+        if not self.__binary:
+            return None
+        if self.__wrapper:
+            command = [self.__wrapper, self.__binary, query]
+        else:
+            command = [self.__binary, query]
+        try:
+            process = Popen(command, stderr = PIPE, stdout = PIPE)
+            out, err = process.communicate()
+            if not process.returncode:
+                lines = out.decode('utf-8').split('\n')
+                if len(lines) > 1:
+                    lines.pop(0)
+                    return lines
+        except:
+            pass
