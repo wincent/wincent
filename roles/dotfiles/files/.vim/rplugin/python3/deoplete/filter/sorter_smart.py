453d5604d113196a8610c79c07b5ec314056bf64
Greg Hurrell <greg@hurrell.net>
2017-06-04 20:18:22 -0700

    vim: plug Command-T fuzzy matcher into deoplete smart sorter

--- roles/dotfiles/files/.vim/rplugin/python3/deoplete/filter/sorter_smart.py
+++ roles/dotfiles/files/.vim/rplugin/python3/deoplete/filter/sorter_smart.py
@@ -1,5 +1,7 @@
 from .base import Base
 
+import score
+
 class Filter(Base):
     def __init__(self, vim):
         Base.__init__(self, vim)
@@ -11,29 +13,6 @@ class Filter(Base):
         complete_str = context['complete_str']
         input_len = len(complete_str)
         smart_case = complete_str.lower() != complete_str
+        # TODO: support smartcase again
         return sorted(context['candidates'],
-                key=lambda x: self.__score(complete_str, x['word'],
-                    smart_case))
-
-    def __score(self, needle, haystack, smart_case):
-        if not smart_case:
-            needle = needle.lower()
-            haystack = haystack.lower()
-        last_match = 0
-        needle_index = 0
-        needle_len = len(needle)
-        score = 0
-        leader = 0
-        seeking = needle[needle_index]
-        for index, char in enumerate(haystack):
-            if char == seeking:
-                if needle_index == 0:
-                    leader = index
-                else:
-                    score += index - last_match
-                last_match = index
-                needle_index += 1
-                if needle_index == needle_len:
-                    break
-                seeking = needle[needle_index]
-        return float('%d.%.4d' % (score, leader))
+                key=lambda x: -1 * score.calc(complete_str, x['word']))
