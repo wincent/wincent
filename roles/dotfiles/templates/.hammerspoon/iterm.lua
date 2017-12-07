50256f3f60b5cc25fbfbc4fb4057dcda1b916184
Greg Hurrell <greg@hurrell.net>
2017-12-06 17:20:18 -0800

    iTerm: use variable for dynamic profile config
    This is more complicated (yuck) but doing it to make the dependency
    among the different places here totally explicit.
    
    It may be a smell to define the variable inside the "iterm" role but
    then reference it from outside in the "dotfiles" role, but I am not sure
    that the alternative of hoisting it up to the global level is much
    better or clearer. At least with this approach we have *some* locality.

---
+++ roles/dotfiles/templates/.hammerspoon/iterm.lua
@@ -0,0 +1,33 @@
+-- {{ ansible_managed }}
+
+--
+-- Switches iTerm dynamic profile based on screen changes.
+--
+
+local events = require 'events'
+local log = require 'log'
+
+-- Forward function declarations.
+local switchProfiles = nil
+
+switchProfiles = (function(screenCount)
+  -- TODO: make this a template so that I can use variables here instead of
+  -- writing all these paths out long-hand.
+  if screenCount == 1 then
+    log.i('Configuring iTerm for Retina (internal) display')
+{% for entry in iterm_dynamic_profiles.retina %}
+    hs.execute("ln -sf \"$HOME/Library/Application Support/iTerm2/Sources/{{ entry.src }}\" \"$HOME/Library/Application Support/iTerm2/DynamicProfiles/{{ entry.dest }}\"")
+{% endfor %}
+  else
+    log.i('Configuring iTerm for 4K (external) display')
+{% for entry in iterm_dynamic_profiles.external %}
+    hs.execute("ln -sf \"$HOME/Library/Application Support/iTerm2/Sources/{{ entry.src }}\" \"$HOME/Library/Application Support/iTerm2/DynamicProfiles/{{ entry.dest }}\"")
+{% endfor %}
+  end
+end)
+
+return {
+  init = (function()
+    events.subscribe('layout', switchProfiles)
+  end),
+}
