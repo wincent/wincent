3f9ab0e89fe1b503910b87a4d03f83dc19fa36d3
Greg Hurrell <greg@hurrell.net>
2018-07-20 10:11:26 -0700

    Remove some work-specific config
    Many of these templates no longer have any conditional forking in them
    and could conceivably go back to being standard files, but for now I
    prefer to make the minimal change as I am not actually going to test
    that this all still works at this point.

--- roles/dotfiles/templates/.mutt/hooks/presync.sh
+++ roles/dotfiles/templates/.mutt/hooks/presync.sh
@@ -2,24 +2,5 @@
 
 # {{ ansible_managed }}
 
-{% if 'personal' in group_names %}
 echo 'No presync checks needed.'
 exit 0
-{% endif %}
-{% if 'work' in group_names %}
-WIFI=$(networksetup -getairportnetwork en0 | awk '{ print $NF }')
-
-if [ "$WIFI" = lighthouse ]; then
-  echo 'On lighthouse: allowing sync.'
-  exit 0
-elif ifconfig utun1 2> /dev/null | grep -q -e '\binet\b'; then
-  echo 'On VPN: allowing sync.'
-  exit 0
-elif ifconfig utun2 2> /dev/null | grep -q -e '\binet\b'; then
-  echo 'On VPN: allowing sync.'
-  exit 0
-else
-  echo 'Not on corporate network: disallowing sync.'
-  exit 1
-fi
-{% endif %}
