46b733022c1455173fe110145526daa2a865ef1c
Greg Hurrell <greg@hurrell.net>
2018-10-26 21:35:47 +0200

    karabiner: add tests for deepCopy()

--- roles/dotfiles/support/karabiner-test.js
+++ roles/dotfiles/support/karabiner-test.js
@@ -5,16 +5,71 @@
  */
 
 const assert = require('assert');
-const {
-  bundleIdentifier,
-  deepCopy,
-  isObject,
-} = require('./karabiner');
+const { bundleIdentifier, deepCopy, isObject } = require('./karabiner');
 
 (function test_bundleIdentifier() {
   (function $() {
     assert(
       bundleIdentifier('com.apple.TextEdit') === '^com\\.apple\\.TextEdit$',
+      $,
+    );
+  })();
+})();
+
+(function test_deepCopy() {
+  const source = {
+    object: {
+      isInner: true,
+    },
+    array: [1, 2, [3, 4]],
+  };
+  const copy = deepCopy(source);
+
+  // Copies look the same.
+  (function $() {
+    assert(
+      JSON.stringify(copy) ===
+        '{"object":{"isInner":true},"array":[1,2,[3,4]]}',
+      $,
+    );
+  })();
+
+  // Objects are cloned.
+  (function $() {
+    assert(
+      source !== copy,
+      $
+    );
+  })();
+
+  // Nested objects are cloned.
+  (function $() {
+    assert(
+      source.object !== copy.object,
+      $
+    );
+  })();
+
+  // Arrays are cloned.
+  (function $() {
+    assert(
+      source.array !== copy.array,
+      $
+    );
+  })();
+
+  // Nested arrays are cloned.
+  (function $() {
+    assert(
+      source.array[2] !== copy.array[2],
+      $
+    );
+  })();
+
+  // Primitives are identical.
+  (function $() {
+    assert(
+      source.array[0] === copy.array[0],
       $
     );
   })();
