5b3fec6bba06cddf2fb6e61a3f8cfbbde65479cc
Greg Hurrell <greg@hurrell.net>
2018-10-26 22:14:44 +0200

    karabiner: add tests for isObject()

--- roles/dotfiles/support/karabiner-test.js
+++ roles/dotfiles/support/karabiner-test.js
@@ -36,41 +36,63 @@ const { bundleIdentifier, deepCopy, isObject } = require('./karabiner');
 
   // Objects are cloned.
   (function $() {
-    assert(
-      source !== copy,
-      $
-    );
+    assert(source !== copy, $);
   })();
 
   // Nested objects are cloned.
   (function $() {
-    assert(
-      source.object !== copy.object,
-      $
-    );
+    assert(source.object !== copy.object, $);
   })();
 
   // Arrays are cloned.
   (function $() {
-    assert(
-      source.array !== copy.array,
-      $
-    );
+    assert(source.array !== copy.array, $);
   })();
 
   // Nested arrays are cloned.
   (function $() {
-    assert(
-      source.array[2] !== copy.array[2],
-      $
-    );
+    assert(source.array[2] !== copy.array[2], $);
   })();
 
   // Primitives are identical.
   (function $() {
-    assert(
-      source.array[0] === copy.array[0],
-      $
-    );
+    assert(source.array[0] === copy.array[0], $);
+  })();
+})();
+
+(function test_isObject() {
+  // Arrays are not objects.
+  (function $() {
+    assert(!isObject([1]), $);
+  })();
+
+  // Booleans are not objects.
+  (function $() {
+    assert(!isObject(true), $);
+  })();
+
+  // `null` is not an object.
+  (function $() {
+    assert(!isObject(null), $);
+  })();
+
+  // Numbers are not objects.
+  (function $() {
+    assert(!isObject(1), $);
+  })();
+
+  // Strings are not objects.
+  (function $() {
+    assert(!isObject('this'), $);
+  })();
+
+  // `undefined` is not an object.
+  (function $() {
+    assert(!isObject(undefined), $);
+  })();
+
+  // Objects are objects.
+  (function $() {
+    assert(isObject({}), $);
   })();
 })();
