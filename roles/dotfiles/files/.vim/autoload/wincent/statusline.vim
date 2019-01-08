wincent/wincent

commit fa55f82c9b47bd1af7ce9d006dcab57c4e4b2794

vim: replace a hand-rolled substitute() call with fnamemodify()

roles/dotfiles/files/.vim/autoload/wincent/statusline.vim
@@ -13,7 +13,7 @@ function! wincent#statusline#fileprefix() abort
     return ''
   else
     " Make sure we show $HOME as ~.
-    return substitute(l:basename . '/', '\C^' . $HOME, '~', '')
+    return fnamemodify(l:basename, ':~:.')
   endif
 endfunction
 

commit f6a83f105801e0fda9a737ddc7793d8739f27365

vim: add back missing trailing slash in statusline
Regression introduced in fa55f82c9b47bd1.

roles/dotfiles/files/.vim/autoload/wincent/statusline.vim
@@ -13,7 +13,7 @@ function! wincent#statusline#fileprefix() abort
     return ''
   else
     " Make sure we show $HOME as ~.
-    return fnamemodify(l:basename, ':~:.')
+    return fnamemodify(l:basename, ':~:.') . '/'
   endif
 endfunction
 