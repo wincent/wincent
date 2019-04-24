2f3a043298a7659cbcad81ba0399d0135b9f354f
Greg Hurrell <greg@hurrell.net>
2019-04-24 09:44:28 +0200

    zsh: teach "portool tail" to retry
    Because the log file may not exist at the time you run it.

--- roles/dotfiles/files/.zsh/host/LFRW044-bin/portool-helpers/.common.sh
+++ roles/dotfiles/files/.zsh/host/LFRW044-bin/portool-helpers/.common.sh
@@ -1,4 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+# Based on: https://stackoverflow.com/a/8351489/2103996
+backoff() {
+  local max_attempts=${ATTEMPTS-5}
+  local timeout=${TIMEOUT-1}
+  local attempt=1
+  local exitCode=0
+
+  while (( $attempt < $max_attempts ))
+  do
+    if "$@"
+    then
+      return 0
+    else
+      exitCode=$?
+    fi
+
+    echo "error: retrying in $timeout..." 1>&2
+    sleep $timeout
+    attempt=$(( attempt + 1 ))
+    timeout=$(( timeout * 2 ))
+  done
+
+  if [[ $exitCode != 0 ]]
+  then
+    echo "error: maximum attempt count exceeded ($@)" 1>&2
+  fi
+
+  return $exitCode
+}
 
 signal_ant() {
   SIG=$1
