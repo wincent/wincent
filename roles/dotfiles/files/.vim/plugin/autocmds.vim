wincent/wincent

commit 92bb42b003d5110e49258aa196e1667361714751

vim: avoid end-of-line on last line in main work repo

roles/dotfiles/files/.vim/plugin/autocmds.vim
@@ -1,3 +1,13 @@
+let g:wincent_override_filetypes=[
+      \   'groovy',
+      \   'html',
+      \   'javascript',
+      \   'jproperties',
+      \   'json',
+      \   'typescript',
+      \   'xml'
+      \ ]
+
 if has('autocmd')
   function! s:WincentAutocmds()
     augroup WincentAutocmds
@@ -58,7 +68,8 @@ if has('autocmd')
       " When we later expand it with ":p", that will make it look like a
       " file (eg. "javascript") in the current directory. Fortunately, this
       " serves our purposes adequately.
-      autocmd FileType html,java,javascript,json,typescript call wincent#autocmds#apply_overrides(expand('<afile>'), expand('<amatch>'))
+      let s:pattern=join(g:wincent_override_filetypes, ',')
+      execute 'autocmd FileType ' . s:pattern . " call wincent#autocmds#apply_overrides(expand('<afile>'), expand('<amatch>'))"
     augroup END
   endfunction
 

roles/dotfiles/files/.vim/autoload/wincent/autocmds.vim
@@ -185,8 +185,10 @@ function! wincent#autocmds#encrypt(file) abort
 endfunction
 
 function! wincent#autocmds#apply_overrides(file, type) abort
-  if match(a:type, '\<\(html\|java\|javascript\|json\|typescript\)\>') != -1
-    if wincent#liferay#detect(a:file)
+  let l:pattern=join(g:wincent_override_filetypes, '\|')
+  if match(a:type, '\<\(' . l:pattern . '\)\>') != -1
+    let l:detected=wincent#liferay#detect(a:file)
+    if l:detected
       setlocal noexpandtab
       setlocal shiftwidth=4
       setlocal tabstop=4
@@ -201,6 +203,12 @@ function! wincent#autocmds#apply_overrides(file, type) abort
         " applying.
         map <buffer> gq <Plug>(operator-format-and-retab)
         call operator#user#define('format-and-retab', 'wincent#autocmds#format')
+      endif
+
+      if l:detected == 2
+        " Additional settings for main liferay-portal repo.
+        setlocal noendofline
+        setlocal nofixendofline
       endif
     endif
   endif

roles/dotfiles/files/.vim/autoload/wincent/liferay.vim
@@ -3,6 +3,7 @@
 let s:directories={}
 
 " Returns 1 if `file` looks like it belongs to a Liferay project.
+" Returns 2 if it looks like it's in the main liferay-portal repo.
 function! wincent#liferay#detect(file) abort
   let l:git=wincent#git#get_git_dir(a:file)
   if l:git == ''
@@ -14,8 +15,13 @@ function! wincent#liferay#detect(file) abort
       let l:liferay=0
       try
         let l:remotes=system('git --git-dir=' . shellescape(l:git) . ' remote -v')
-        let l:match=match(l:remotes, '\<liferay\>')
-        let l:liferay=(l:match != -1)
+        let l:match=match(l:remotes, '\<liferay-portal\>')
+        if l:match != -1
+          let l:liferay=2
+        else
+          let l:match=match(l:remotes, '\<liferay\>')
+          let l:liferay=(l:match != -1)
+        endif
       catch
         "
       endtry

commit 44f3d8c8dd9f59514992d4cf4c57eb42877dc704

fix(vim): reapply work-specific overrides to Java files
I set up overrides in de4838a563c5614f59c165de4d3afb74ea4e872e but the
refactoring to make things more general in 92bb42b003d5110e49258aa196e1
accidentally dropped "java" from the file-types list.

roles/dotfiles/files/.vim/plugin/autocmds.vim
@@ -4,6 +4,7 @@ let g:wincent_override_filetypes=[
       \   'conf',
       \   'groovy',
       \   'html',
+      \   'java',
       \   'javascript',
       \   'jproperties',
       \   'json',