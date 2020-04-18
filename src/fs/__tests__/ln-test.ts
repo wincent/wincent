267219a2af98a5ccf23e36f8fe516246d8d84788
Greg Hurrell <greg@hurrell.net>
2020-04-18 21:09:36 +0200

    test(fig): add `ln()` tests

---
+++ src/fs/__tests__/ln-test.ts
@@ -0,0 +1,74 @@
+import {join} from 'path';
+
+import assert from '../../assert.js';
+import {expect, test} from '../../test/harness.js';
+import ln from '../ln.js';
+import stat from '../stat.js';
+import tempdir from '../tempdir.js';
+import touch from '../touch.js';
+
+test('ln() links a file', async () => {
+    const base = await tempdir('ln-test');
+
+    const source = join(base, 'source.txt');
+
+    const target = join(base, 'link.txt');
+
+    await touch(source);
+
+    const result = await ln(source, target);
+
+    expect(result).toBe(null);
+
+    const stats = await stat(target);
+    assert(!(stats instanceof Error));
+
+    assert(stats !== null);
+
+    expect(stats.type).toBe('link');
+    expect(stats.target).toBe(source);
+});
+
+test('ln() with `{force: true}` overwrites if necessary', async () => {
+    const base = await tempdir('ln-test');
+
+    const source = join(base, 'source.txt');
+
+    const target = join(base, 'link.txt');
+
+    await touch(source);
+    await touch(target);
+
+    const result = await ln(source, target, {force: true});
+
+    expect(result).toBe(null);
+
+    const stats = await stat(target);
+    assert(!(stats instanceof Error));
+
+    assert(stats !== null);
+
+    expect(stats.type).toBe('link');
+    expect(stats.target).toBe(source);
+});
+
+test('ln() can create links to non-existent files', async () => {
+    // Just link `ln` can...
+    const base = await tempdir('ln-test');
+
+    const source = join(base, 'source.txt');
+
+    const target = join(base, 'link.txt');
+
+    const result = await ln(source, target);
+
+    expect(result).toBe(null);
+
+    const stats = await stat(target);
+    assert(!(stats instanceof Error));
+
+    assert(stats !== null);
+
+    expect(stats.type).toBe('link');
+    expect(stats.target).toBe(source);
+});
