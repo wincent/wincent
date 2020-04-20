67213bd382c69d9c79551ec532757973ecf254c3
Greg Hurrell <greg@hurrell.net>
2020-04-21 00:53:06 +0200

    fix(fig): deal with chdir being String
    Because, surprise, surprise, Nodes child_process API will complain with
    this beautiful error:
    
        TypeError [ERR_INVALID_ARG_TYPE]: The "options.cwd" property must be
        of type string. Received an instance of String

--- src/Fig/operations/command.ts
+++ src/Fig/operations/command.ts
@@ -1,7 +1,8 @@
 import ErrorWithMetadata from '../../ErrorWithMetadata.js';
-import {debug} from '../../console/index.js';
+import {debug, log} from '../../console/index.js';
 import path from '../../path.js';
 import spawn from '../../spawn.js';
+import stringify from '../../stringify.js';
 import stat from '../../fs/stat.js';
 import Context from '../Context.js';
 
@@ -32,13 +33,16 @@ export default async function command(
     }
 
     try {
+        log.debug(
+            `Run command \`${description}\` with options: ${stringify(options)}`
+        );
+
         await spawn(
             path(executable).expand,
             args.map((arg) => path(arg).expand),
-            options.chdir ? {cwd: options.chdir} : {}
+            options.chdir ? {cwd: options.chdir.toString()} : {}
         );
 
-        // TODO: decide whether to log full command here
         Context.informChanged(`command \`${description}\``);
     } catch (error) {
         if (error instanceof ErrorWithMetadata) {
