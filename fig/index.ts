c8a44c45715047976d3e23db0f99cbc32e80d352
Greg Hurrell <greg@hurrell.net>
2020-04-24 00:37:07 +0200

    refactor(fig): further simplify exports

--- fig/index.ts
+++ fig/index.ts
@@ -1,27 +1,14 @@
-import {default as attributes} from './dsl/attributes.js';
-import {default as command} from './dsl/operations/command.js';
-import {default as fetch} from './dsl/operations/fetch.js';
-import {default as file} from './dsl/operations/file.js';
-import {default as template} from './dsl/operations/template.js';
-import * as resource from './dsl/resource.js';
-import {default as root} from './dsl/root.js';
-import {default as skip} from './dsl/skip.js';
-import {default as task} from './dsl/task.js';
-import {default as variable} from './dsl/variable.js';
-import {default as variables} from './dsl/variables.js';
-
 // TODO: maybe make a "register" function for arbitrary communication/sharing
 // across tasks/aspects
 // TODO: export "path" here for convenience? it is very DSL-ish
-
-export {attributes};
-export {command};
-export {fetch};
-export {file};
-export {resource};
-export {root};
-export {skip};
-export {task};
-export {template};
-export {variable};
-export {variables};
+export {default as attributes} from './dsl/attributes.js';
+export {default as command} from './dsl/operations/command.js';
+export {default as fetch} from './dsl/operations/fetch.js';
+export {default as file} from './dsl/operations/file.js';
+export {default as template} from './dsl/operations/template.js';
+export * as resource from './dsl/resource.js';
+export {default as root} from './dsl/root.js';
+export {default as skip} from './dsl/skip.js';
+export {default as task} from './dsl/task.js';
+export {default as variable} from './dsl/variable.js';
+export {default as variables} from './dsl/variables.js';
