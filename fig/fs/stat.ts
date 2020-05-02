df76caf27abfa0f6a3e5626becb96dcd0a0b26b7
Greg Hurrell <greg@hurrell.net>
2020-05-02 19:38:06 +0200

    feat(fig): teach "stat.ts" to work on Linux
    Untested...

--- fig/fs/stat.ts
+++ fig/fs/stat.ts
@@ -30,6 +30,10 @@ const TYPE_MAP = {
 export default async function stat(
     path: string
 ): Promise<Error | Stats | null> {
+    const target = toPath(path).expand;
+
+    const args = [];
+
     if (Context.attributes.platform === 'darwin') {
         const formats = {
             group: '%Sg',
@@ -48,45 +52,8 @@ export default async function stat(
             formats.target,
         ].join(formats.newline);
 
-        // Try without sudo, then with.
-        for (const sudo of [false, true]) {
-            const options = sudo
-                ? {passphrase: await Context.sudoPassphrase}
-                : undefined;
-
-            const target = toPath(path).expand;
-
-            const {status, stderr, stdout} = await run(
-                'stat',
-                ['-f', formatString, target],
-                options
-            );
-
-            if (status === 0) {
-                const [mode, type, owner, group, target] = stdout.split('\n');
-
-                const paddedMode = mode.padStart(4, '0');
-
-                assertMode(paddedMode);
-
-                return {
-                    group,
-                    mode: paddedMode,
-                    target: target || undefined,
-                    type: (TYPE_MAP as any)[type.toLowerCase()] || 'unknown',
-                    owner,
-                };
-            }
-
-            if (/no such file/i.test(stderr)) {
-                return null;
-            } else if (!/permission denied/i.test(stderr)) {
-                // Give up...
-                break;
-            }
-        }
-    } else {
-        throw new Error('TODO: implement');
+        args.push('-f', formatString, target);
+    } else if (Context.attributes.platform === 'linux') {
         // GNU: command stat --printf '%a\n%F\n%G\n%U\n'
         // a = mode
         // F = type
@@ -95,6 +62,75 @@ export default async function stat(
         // maybe %N (link target): prints 'src' -> 'target'
         // 644, 1777
         // regular file, directory, symbolic link, character special file
+        const formats = {
+            group: '%G',
+            mode: '%a',
+            newline: '\\n',
+            target: '%N',
+            type: '%F',
+            owner: '%U',
+        };
+
+        const formatString =
+            [
+                formats.mode,
+                formats.type,
+                formats.owner,
+                formats.group,
+                formats.target,
+            ].join(formats.newline) + '\\n';
+
+        args.push('-c', formatString, target);
+    }
+
+    // Try without sudo, then with.
+    for (const sudo of [false, true]) {
+        const options = sudo
+            ? {passphrase: await Context.sudoPassphrase}
+            : undefined;
+
+        const {status, stderr, stdout} = await run('stat', args, options);
+
+        if (status === 0) {
+            const [mode, type, owner, group, target] = stdout.split('\n');
+
+            const paddedMode = mode.padStart(4, '0');
+
+            assertMode(paddedMode);
+
+            let parsedTarget: string | undefined = undefined;
+
+            if (target) {
+                if (Context.attributes.platform === 'darwin') {
+                    parsedTarget = target;
+                } else if (Context.attributes.platform === 'linux') {
+                    parsedTarget = target.split(' -> ')[1];
+
+                    if (parsedTarget) {
+                        parsedTarget = parsedTarget
+                            .replace(/^'/, '')
+                            .replace(/'$/, '')
+                            .replace(/\\'/, "'")
+                            .replace(/\\\\'/, '\\');
+                    }
+                }
+            }
+
+            return {
+                group,
+                mode: paddedMode,
+                target: parsedTarget,
+                type: (TYPE_MAP as any)[type.toLowerCase()] || 'unknown',
+                owner,
+            };
+        }
+
+        if (/no such file/i.test(stderr)) {
+            return null;
+        } else if (!/permission denied/i.test(stderr)) {
+            // Give up...
+            break;
+        }
     }
 
     return new ErrorWithMetadata(`Unable to stat ${path}`);
