7ce8f4980ed3a3c5f278974a39a9938a45a2215f
Greg Hurrell <greg@hurrell.net>
2020-05-02 19:39:42 +0200

    chore(fig): let chown run on Linux
    AFAIK, it will work.

--- fig/posix/chown.ts
+++ fig/posix/chown.ts
@@ -14,45 +14,41 @@ export default async function chown(
     path: string,
     options: Options = {}
 ): Promise<Error | null> {
-    if (Context.attributes.platform === 'darwin') {
-        // Run one of:
-        //
-        //      chown owner path
-        //      chown :group path
-        //      chown owner:group path
-        //
-        // With or without `sudo` (although note that, in practice, if
-        // we are calling `chown()` at all, it will probably be with
-        // `sudo`).
-        //
+    // Run one of:
+    //
+    //      chown owner path
+    //      chown :group path
+    //      chown owner:group path
+    //
+    // With or without `sudo` (although note that, in practice, if
+    // we are calling `chown()` at all, it will probably be with
+    // `sudo`).
+    //
 
-        let ownerAndGroup = options.owner || '';
+    let ownerAndGroup = options.owner || '';
 
-        if (options.group) {
-            ownerAndGroup += `:${options.group}`;
-        }
+    if (options.group) {
+        ownerAndGroup += `:${options.group}`;
+    }
 
-        const args = [ownerAndGroup, path];
+    const args = [ownerAndGroup, path];
 
-        const passphrase = options.sudo
-            ? await Context.sudoPassphrase
-            : undefined;
+    const passphrase = options.sudo
+        ? await Context.sudoPassphrase
+        : undefined;
 
-        log.debug(`Setting ownership: ${args.join(' ')}`);
+    log.debug(`Setting ownership: ${args.join(' ')}`);
 
-        const result = await run('chown', args, {passphrase});
+    const result = await run('chown', args, {passphrase});
 
-        if (result.status === 0) {
-            return null;
-        } else {
-            log.debug(stringify(result));
-
-            return (
-                result.error ||
-                new ErrorWithMetadata(`\`chown ${args.join(' ')}\` failed`)
-            );
-        }
+    if (result.status === 0) {
+        return null;
     } else {
-        throw new Error('TODO: implement');
+        log.debug(stringify(result));
+
+        return (
+            result.error ||
+            new ErrorWithMetadata(`\`chown ${args.join(' ')}\` failed`)
+        );
     }
 }
