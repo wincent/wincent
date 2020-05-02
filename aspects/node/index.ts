2b0d6c89eaa208565a86ba642f11c23b1cfcbd25
Greg Hurrell <greg@hurrell.net>
2020-05-02 17:21:52 +0200

    feat(node): install global packages
    "Global" in the sense that they go into `~/n`.
    
    Not sure if there are any chicken-and-egg problems with respect to
    Neovim, but even if there are, they probably don't run very deep (ie. I
    am not using and JS-based plug-ins at this time; only stuff that
    interacts indirectly with the rig, like typescript-language-server).

--- aspects/node/index.ts
+++ aspects/node/index.ts
@@ -1,12 +1,11 @@
-import {command, file, task} from 'fig';
+import {command, file, skip, task} from 'fig';
 import path from 'fig/path.js';
 
-const env = {
-    ...process.env,
-    N_PREFIX: path.home.join('n'),
-};
+const NODE_VERSION = '10.16.0';
 
 const n = path.root.join('vendor/n/bin/n');
+const bin = path.home.join(`n/bin`);
+const npm = bin.join('npm');
 
 task('make ~/n', async () => {
     await file({path: '~/n', state: 'directory'});
@@ -16,21 +15,49 @@ task('hide ~/n', async () => {
     await command('chflags', ['hidden', '~/n']);
 });
 
-task('install Node.js v10.16.0', async () => {
-    await command(n, ['10.16.0'], {
-        creates: '~/n/n/versions/node/10.16.0/bin/node',
+task(`install Node.js v${NODE_VERSION}`, async () => {
+    const env = {
+        ...process.env,
+        N_PREFIX: path.home.join('n'),
+    };
+
+    await command(n, [NODE_VERSION], {
+        creates: `~/n/n/versions/node/${NODE_VERSION}/bin/node`,
         env,
     });
 });
 
-// TODO install global packages
-// - bs-platform
-// - flow-bin
-// - flow-typed
-// - gatsby-cli
-// - javascript-typescript-langserver
-// - neovim
-// - ocaml-language-server
-// - reason-cli
-// - source-map-explorer
-// - typescript
+task('install global packages', async () => {
+    const env = {
+        ...process.env,
+        N_PREFIX: path.home.join('n'),
+        PATH: `${bin}:${process.env.PATH}`,
+    };
+
+    const packages = [
+        'bs-platform',
+        'flow-bin',
+        'flow-typed',
+        'gatsby-cli',
+        'javascript-typescript-langserver',
+        'neovim',
+        'ocaml-language-server',
+        'reason-cli',
+        'source-map-explorer',
+        'typescript',
+        'yarn@1.22.4',
+    ];
+
+    for (const name of packages) {
+        const result = await command(npm, ['ls', '-g', '--parseable', name], {
+            env,
+            failedWhen: () => false,
+        });
+
+        if (result && result.status === 0) {
+            skip(`package ${name} (already installed)`);
+        } else {
+            await command(npm, ['install', '-g', name], {env});
+        }
+    }
+});
