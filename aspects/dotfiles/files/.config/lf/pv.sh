e9e42d80edff3b5d074eb8aff8c75750f1aff66e
Greg Hurrell <greg@hurrell.net>
2020-05-27 02:20:20 +0200

    feat(lf): use MIME types in preview script as well

--- aspects/dotfiles/files/.config/lf/pv.sh
+++ aspects/dotfiles/files/.config/lf/pv.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-shopt -s nocasematch
-
 FILE="$1"
 HEIGHT="$2"
 
 # TODO: figure out how to avoid hard-coding this; (`tput cols` always returns 80)
 WIDTH=80
 
-case "$1" in
-  *.gif|*.jpeg|*.jpg|*.png)
+case $(file --brief --mime-type "$FILE") in
+  image/*)
     chafa --fill=block --symbols=block --colors=256 --size="$WIDTH"x"$HEIGHT" "$FILE"
     ;;
+  text/*)
+    highlight --force --out-format=ansi -- "$FILE"
+    ;;
   *)
-    highlight --force --out-format=ansi -- "$FILE" || file -b "$FILE"
+    file --brief "$FILE"
     ;;
 esac
