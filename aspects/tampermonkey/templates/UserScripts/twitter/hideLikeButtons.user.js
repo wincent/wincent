28ff5254f5a2888c74e8c06a62f73b6f0528f168
Greg Hurrell <greg@hurrell.net>
2020-06-12 15:31:31 +0200

    feat(tampermonkey): hide Twitter "Like" buttons
    Because:
    
    https://twitter.com/wincent/status/1271365891234705408
    
    I don't want me clicking "Like" on something to take it as a cue to
    publish something with my name next to it.
    
    This will help me avoid clicking on this button accidentally.

---
+++ aspects/tampermonkey/templates/UserScripts/twitter/hideLikeButtons.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Hide Twitter Like buttons
+// @namespace    https://wincent.com/
+// @version      0.2
+// @description  Hide Twitter Like buttons
+// @author       Greg Hurrell <greg@hurrell.net>
+// @match        https://twitter.com/*
+// @grant        none
+// @downloadURL  http://localhost/~<%= variables.username %>/UserScripts/twitter/hideLikeButtons.user.js
+// @updateURL    http://localhost/~<%= variables.username %>/UserScripts/twitter/hideLikeButtons.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const style = document.createElement('style');
+
+    style.innerHTML = `
+        [aria-label~=Like][role=button] {
+            visibility: hidden;
+        }
+    `;
+
+    document.head.appendChild(style);
+
+})();
+
+// <%= variables.figManaged %>
