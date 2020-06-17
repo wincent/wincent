be16f52804cf03a72aeb5fe69e6a0159615df5b4
Greg Hurrell <greg@hurrell.net>
2020-06-17 17:27:00 +0200

    feat(tampermonkey): remove annoying purple highlighting in Gmail
    They add spans around text that they consider duplicate:
    
        .im {
            color: #500050;
        }
    
    Ugh...

---
+++ aspects/tampermonkey/templates/UserScripts/gmail/removeHighlighting.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Remove annoying purple color added to text by Gmail
+// @namespace    https://wincent.com/
+// @version      0.1
+// @description  Remove annoying purple color added to text by Gmail
+// @author       Greg Hurrell <greg@hurrell.net>
+// @match        https://mail.google.com/mail/*
+// @grant        none
+// @downloadURL  http://localhost/~<%= variables.username %>/UserScripts/gmail/removeHighlighting.user.js
+// @updateURL    http://localhost/~<%= variables.username %>/UserScripts/gmail/removeHighlighting.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const style = document.createElement('style');
+
+    style.innerHTML = `
+        .im {
+            color: initial;
+        }
+    `;
+
+    document.head.appendChild(style);
+})();
+
+// <%= variables.figManaged %>
