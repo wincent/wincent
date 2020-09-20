9bd4601d7888650f8e5668e27e13fde99c112969
Greg Hurrell <greg@hurrell.net>
2020-09-20 23:46:45 +0200

    feat(shell): set user shell

--- aspects/shell/index.ts
+++ aspects/shell/index.ts
@@ -1,11 +1,33 @@
-import {line, task} from 'fig';
+import {attributes, command, line, skip, task, variable} from 'fig';
 
 task('add /usr/local/bin/zsh to /etc/shells', async () => {
-    await line({
-        group: 'wheel',
-        line: '/usr/local/bin/zsh',
-        owner: 'root',
-        path: '/etc/shells',
-        sudo: true,
-    });
+    if (attributes.distribution === 'arch') {
+        skip('no need to touch /etc/shells on Arch');
+    } else {
+        await line({
+            group: 'wheel',
+            line: '/usr/local/bin/zsh',
+            owner: 'root',
+            path: '/etc/shells',
+            sudo: true,
+        });
+    }
+});
+
+task('set user shell to /bin/zsh', async () => {
+    if (variable('identity') === 'wincent') {
+        if (attributes.distribution === 'arch') {
+            await command('chsh', ['-s', '/bin/zsh', attributes.username], {
+                sudo: true,
+            });
+        } else {
+            await command(
+                'chsh',
+                ['-s', '/usr/local/bin/zsh', attributes.username],
+                {sudo: true}
+            );
+        }
+    } else {
+        skip();
+    }
 });
