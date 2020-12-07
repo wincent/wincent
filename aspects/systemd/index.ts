325e708f23dc38f037b04fe3c62c4c89af621eca
Greg Hurrell <greg@hurrell.net>
2020-12-07 10:56:18 +0100

    style(systemd): fix formatting

--- aspects/systemd/index.ts
+++ aspects/systemd/index.ts
@@ -4,7 +4,10 @@ task('set up hostname', async () => {
     const hostname = variable.string('hostname');
     const result = await command('hostname', []);
 
-    if (variable('identity') === 'wincent' && result!.stdout.trim() !== hostname) {
+    if (
+        variable('identity') === 'wincent' &&
+        result!.stdout.trim() !== hostname
+    ) {
         await command('hostnamectl', ['set-hostname', hostname]);
     } else {
         skip();
