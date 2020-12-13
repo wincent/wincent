fa0da72d8175f85027ce59e95e3a25cfa7571148
Greg Hurrell <greg@hurrell.net>
2020-12-14 00:31:29 +0100

    docs(dotfiles): show how to make xkeysnail work with grabbed device
    ie. by grabbing clone instead.
    
    This makes things work in Chromium even when udevmon is already active.
    However, it doesn't pass keys through to other apps; I think I might
    need something additional for that (or maybe just turn the udevmon
    service off; you can achieve a similar result with this xkeysnail,
    although it won't work on virtual consoles because I believe it's tied
    to X.

--- aspects/dotfiles/files/.config/xkeysnail.py
+++ aspects/dotfiles/files/.config/xkeysnail.py
@@ -1,17 +1,29 @@
 # Sadly, this isn't compatible with the udevmon.service; if you try to test it:
 #
-#    sudo xkeysnail ~/.config/xkeysnail.py
+#       sudo xkeysnail ~/.config/xkeysnail.py
 #
 # it will die with:
 #
-#    IOError when grabbing device. Maybe, another xkeysnail instance is running?
+#       IOError when grabbing device. Maybe, another xkeysnail instance is running?
 #
 # To see it working, first do:
 #
-#    systemctl stop udevmon
+#       systemctl stop udevmon
+#
+# Or specify a `--device` explicitly:
+#
+#       sudo xkeysnail ~/.config/xkeysnail.py --devices /dev/input/event18
+#
+# ie. the cloned virtual device exposed by Interception tools:
+#
+#       https://gitlab.com/interception/linux/tools
 #
 import re
 from xkeysnail.transform import *
+
+# For key names, see:
+#
+#       https://github.com/mooz/xkeysnail/blob/master/xkeysnail/key.py
 
 define_keymap(re.compile("chromium", re.IGNORECASE), {
     K("M-LEFT_BRACE"): K("C-Shift-TAB"),
