5fe7b1980073e53b04f4b4089c1c413356ade7d7
Greg Hurrell <greg@hurrell.net>
2020-12-14 00:15:40 +0100

    feat(dotfiles): provide sample xkeysnail config
    As noted in the comment though, I don't think I can use this with the
    udevmon service as they both try to grab the device.
    
    I might be able to implement something similar-ish using Interception
    tools though (basically add something that checks X to see the frontmost
    application; not sure whether that will be prohibitively slow or not
    though...).

---
+++ aspects/dotfiles/files/.config/xkeysnail.py
@@ -0,0 +1,19 @@
+# Sadly, this isn't compatible with the udevmon.service; if you try to test it:
+#
+#    sudo xkeysnail ~/.config/xkeysnail.py
+#
+# it will die with:
+#
+#    IOError when grabbing device. Maybe, another xkeysnail instance is running?
+#
+# To see it working, first do:
+#
+#    systemctl stop udevmon
+#
+import re
+from xkeysnail.transform import *
+
+define_keymap(re.compile("chromium", re.IGNORECASE), {
+    K("M-LEFT_BRACE"): K("C-TAB"),
+    K("M-RIGHT_BRACE"): K("C-Shift-TAB")
+}, "Chrom
