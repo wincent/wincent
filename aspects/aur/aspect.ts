2a9e6b5628e7e44565de4515069a190704ca076f
Greg Hurrell <greg@hurrell.net>
2020-12-14 21:00:23 +0100

    refactor(aur): port aspect from JSON to TS
    So that I can add comment to it like I did in the pacman aspect.

---
+++ aspects/aur/aspect.ts
@@ -0,0 +1,12 @@
+export default {
+    description: 'Installs and updates packages from the Arch User Repository',
+    variables: {
+        packages: [
+            'clipper-git', // Clipboard daemon.
+            'interception-dual-function-keys', // Substitute for some key (ha!) Karabiner-Elements functionality.
+            'lf', // Command-line file explorer.
+            'neovim-nightly', // Nightnly Neovim for access to latest LSP goodness.
+            'xkeysnail', // Per-application keyboard overrides.
+        ],
+    },
+};
