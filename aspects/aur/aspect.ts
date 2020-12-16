5fa2cec99426136c98c5416ad3bed87d59365410
Greg Hurrell <greg@hurrell.net>
2020-12-17 00:44:04 +0100

    fix(aur): add ttf-symbola to add missing glyph for prompt

--- aspects/aur/aspect.ts
+++ aspects/aur/aspect.ts
@@ -6,6 +6,7 @@ export default {
             'interception-dual-function-keys', // Substitute for some key (ha!) Karabiner-Elements functionality.
             'lf', // Command-line file explorer.
             'neovim-nightly', // Nightnly Neovim for access to latest LSP goodness.
+            'ttf-symbola', // Font with lots of Unicode glyphs (eg. u276f, for prompt).
             'xkeysnail', // Per-application keyboard overrides.
         ],
     },
