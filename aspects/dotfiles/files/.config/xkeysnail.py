5e1e41b64599287484e31db95163195cb7a8d44a
Greg Hurrell <greg@hurrell.net>
2020-12-20 13:28:06 +0100

    chore(dotfiles): continue experimenting with macOS-like keybindings
    So still some noted annoyances with this experiment, which is why it
    remains an experiment at this point, plus some things I don't
    understand.
    
    - xkeysnail has to run as root from user context (can't do
      per-application overrides with Interception Tools because they run
      "below" X).
    
    - xkeysnail falls over whenever there are any device changes (eg.
      display sleep...) requiring a restart.
    
    - Several combinations not working for unknown reasons. For example,
      Chrome "grabbing" Alt-F before xkeysnail sees it; apprently there is a
      setting in Firefox to disable this, but not Chrome; see:
    
          https://github.com/mooz/xkeysnail/issues/43
    
    - There is some weirdness I don't understand with my "C-I" remapping in
      Kitty. Other keys seems to want the Colemak key names (eg. "Alt-G"
      correctly maps "Search again") but here where I would want to map
      "C-I", that doesn't work; it only works if I target "C-L" (ie. "L" is the
      physical key on the Qwerty keyboard that you have to press in order to
      get "I" on Colemak).
    
    Next steps: see if I can move some of this down to the more reliable
    layer of Interception Tools; only the per-application stuff need remain
    with xkeysnail. Also, keep massaging i3 config to make sure it feels
    good and doesn't collide with any of the other GUI-level stuff.
    
    https://docs.google.com/spreadsheets/d/1gYenqlrgZoWbqv-_ciO2XhBIBAFuP7i2jrKm7J9xLVE/edit?usp=sharing

--- aspects/dotfiles/files/.config/xkeysnail.py
+++ aspects/dotfiles/files/.config/xkeysnail.py
@@ -18,6 +18,16 @@
 #
 #       https://gitlab.com/interception/linux/tools
 #
+# Some annoyances with this solution:
+#
+# -   xkeysnail may consider the input device to be disconnected on waking from
+#     sleep (eg. see: https://gitlab.com/interception/linux/plugins/caps2esc,
+#     "It resets any time a device change happens (bluetooth, usb, any) or the
+#     laptop lid is closed or when logging off and needs to be re-executed.")
+# -   Have to jump through hoops because of need to run as root.
+# -   A number of combinrations don't seem to work.
+# -   Python dependency graph.
+#
 import re
 from xkeysnail.transform import *
 
@@ -25,16 +35,45 @@ from xkeysnail.transform import *
 #
 #       https://github.com/mooz/xkeysnail/blob/master/xkeysnail/key.py
 
+define_keymap(None, {
+    K("M-A"): K("C-A"), # Select all.
+    K("M-C"): K("C-C"), # Copy.
+    K("M-Left"): K("Home"), # Start of line. TODO: make this work in kitty although I probably won't use it.
+    K("M-Right"): K("End"), # End of line. TODO: make this work in kitty although I probably won't use it.
+    K("M-Shift-V"): K("C-Shift-V"), # Paste and match style.
+    K("M-Shift-Z"): K("C-Shift-Z"), # Redo.
+    K("M-X"): K("C-X"), # Cut.
+    K("M-Z"): K("C-Z"), # Undo.
+    K("Super-Left"): K("C-Left"), # Previous word. TODO: make this work in kitty.
+    K("Super-Right"): K("C-Right"), # Next word. TODO: make this work in kitty.
+});
+
 define_keymap(re.compile("chromium", re.IGNORECASE), {
-    K("M-F"): K("C-F"), # Doesn't work.
-    K("M-G"): K("C-G"),
-    K("M-L"): K("C-L"), # Doesn't work.
-    K("M-Shift-LEFT_BRACE"): K("C-Shift-TAB"),
-    K("M-N"): K("C-N"), # Doesn't work.
-    K("M-Shift-RIGHT_BRACE"): K("C-TAB"),
-    K("M-Shift-G"): K("C-Shift-G"),
-    K("M-Shift-N"): K("C-Shift-N"), # Doesn't work.
-    K("M-Shift-T"): K("C-Shift-T"), # Doesn't work.
-    K("M-T"): K("C-T"),
-    K("M-W"): K("C-W"),
+    K("M-EQUAL"): K("C-EQUAL"), # Zoom in.
+    K("M-F"): K("C-F"), # Search (doesn't work; opens menu). TODO: do this unconditionally with Interception Tools?
+    K("M-G"): K("C-G"), # Search again.
+    K("M-L"): K("C-L"), # Focus location bar (doesn't work); https://www.chromium.org/user-experience/keyboard-access
+    K("M-LEFT_BRACE"): K("Alt-Left"), # Previous page.
+    K("M-MINUS"): K("C-MINUS"), # Zoom out.
+    K("M-Shift-LEFT_BRACE"): K("C-Shift-TAB"), # Previous tab.
+    K("M-N"): K("C-N"), # New window (doesn't work; actually, does work, sometimes...).
+    K("M-RIGHT_BRACE"): K("Alt-Right"), # Next page.
+    K("M-R"): K("C-R"), # Reload (doesn't work).
+    K("M-Shift-RIGHT_BRACE"): K("C-TAB"), # Next tab.
+    K("M-Shift-G"): K("C-Shift-G"), # Previous search.
+    K("M-Shift-N"): K("C-Shift-N"), # New incognito window (doesn't work).
+    K("M-Shift-T"): K("C-Shift-T"), # Re-open closed tab (doesn't work).
+    K("M-T"): K("C-T"), # New tab.
+    K("M-W"): K("C-W"), # Close tab.
+    K("Super-Alt-I"): K("C-Shift-I"), # Toggle Developer Tools (doesn't work).
+    K("Super-Alt-J"): K("C-Shift-J"), # Toggle JavaScript console (doesn't work).
 }, "Chromium")
+
+define_keymap(re.compile("kitty", re.IGNORECASE), {
+    K("C-L"): K("F6"), # Avoid confusion between Tab and C-I in Vim; note "C-L" (Qwerty) corresponds to "C-I" (Colemak).
+    K("M-V"): K("Shift-INSERT"), # Paste.
+}, "Kitty");
+
+define_keymap(lambda wm_class: wm_class not in ("kitty"), {
+    K("M-V"): K("C-V"), # Paste.
+}, "non-Kitty overrides");
