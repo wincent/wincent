90b89219853cc0ed8d094a3e0cafba291543de2d
Greg Hurrell <greg@hurrell.net>
2020-12-24 16:53:31 +0100

    refactor(dotfiles): strip out stale comment

--- aspects/dotfiles/files/.config/i3/mac-paste.sh
+++ aspects/dotfiles/files/.config/i3/mac-paste.sh
@@ -3,8 +3,6 @@
 ACTIVE_WINDOW=$(xdotool getactivewindow)
 KITTY_WINDOWS=$(xdotool search --class kitty)
 
-# BUG: works fine # when called manually from CLI, but not when called from i3
-
 if [ -n "${ACTIVE_WINDOW}" ]; then
   if [[ "${KITTY_WINDOWS[@]}" =~ "${ACTIVE_WINDOW}" ]]; then
     xdotool key --delay 12 ctrl+shift+v
