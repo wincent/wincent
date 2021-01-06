967c6f6fb0dc579af5619031aa43d1da57c8b697
Greg Hurrell <greg@hurrell.net>
2021-01-07 00:02:09 +0100

    chore(aur): switch from neovim-nightly to neovim-git
    Because "neovim-nightly" is apparently no longer a thing.

--- aspects/aur/aspect.ts
+++ aspects/aur/aspect.ts
@@ -6,7 +6,7 @@ export default {
             'clipper-git', // Clipboard daemon.
             'it87-dkms-git', // Allows `sensors` to pick up on it8688 chipset.
             'lf', // Command-line file explorer.
-            'neovim-nightly', // Nightnly Neovim for access to latest LSP goodness.
+            'neovim-git', // Neovim with latest LSP goodness.
             'obs-studio-git', // Open Broadcaster Software (OBS) for streaming/screen-casting.
             'ttf-symbola', // Font with lots of Unicode glyphs (eg. u276f, for prompt).
         ],
