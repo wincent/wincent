be850954a3420469b277a7c915c248866c54089b
Greg Hurrell <greg@hurrell.net>
2021-05-25 11:49:15 +0200

    refactor(pacman): switch to some more lightweight tools
    Instead of `gnome-control-center` (massive overkill), use the hideously
    ugly but simple `pavucontrol` to control PulseAudio.
    
    Get rid of `caja` (can just live with `pcmanfm` instead; it's ugly, but
    it's ridiculously fast and lean).

--- aspects/pacman/aspect.ts
+++ aspects/pacman/aspect.ts
@@ -6,7 +6,6 @@ export default {
       'avahi', // zeroconf ("Bonjour") networking.
       'bluez', // Bluetooth stack.
       'bluez-utils', // Provides `bluetoothctl` CLI tool for pairing etc (see: https://wiki.archlinux.org/index.php/Bluetooth).
-      'caja', // File manager from MATE desktop.
       'chafa', // Low-res character-based image previewer.
       'clang', // Compiler toolchain (eg. used for Neovim LSP).
       'cmake', // Build tool (eg. used to build Neovim).
@@ -15,7 +14,6 @@ export default {
       'dunst', // Notification daemon (eg. for clock dropdown in status bar).
       'feh', // Sets desktop background.
       'git', // Version control system.
-      'gnome-control-center', // For controlling audio (etc).
       'gnome-icon-theme', // eg. for clock icon in dunst notifications.
       'happy', // Haskell golden testing library (needed for docvim).
       'htop', // Fancier process monitor.
@@ -35,9 +33,10 @@ export default {
       'npm', // Node package manager.
       'openssh', // SSH tools.
       'otf-font-awesome', // Icon font (eg. for status bar).
+      'pavucontrol', // Simple PulseAudio configuration UI.
       'pcmanfm', // File manager from LXDE desktop (lightweight).
       'picom', // X compositor (eg. window transparency etc).
-      'pulseaudio-bluetooth',
+      'pulseaudio-bluetooth', // Bluetooth headset support for PulseAudio.
       'python-neovim', // Support for running Python plugins in Neovim.
       'python-pip', // So we can install commandt.score and redis (for deoplete plugins).
       'ripgrep', // Grep replacement.
