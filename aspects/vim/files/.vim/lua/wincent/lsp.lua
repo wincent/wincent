20b7d524dee4038b6b22694dfb58b2f1f0986a47
Greg Hurrell <greg@hurrell.net>
2021-05-26 23:41:45 +0200

    feat(vim): also apply lspsaga treatment to line diagnostics windows
    Makes them prettier, thanks to the border.

--- aspects/vim/files/.vim/lua/wincent/lsp.lua
+++ aspects/vim/files/.vim/lua/wincent/lsp.lua
@@ -6,7 +6,7 @@ end
 
 local on_attach = function ()
   local mappings = {
-    ['<Leader>ld'] = '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+    ['<Leader>ld'] = "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>",
     ['<c-]>'] = '<cmd>lua vim.lsp.buf.definition()<CR>',
     ['K'] = "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>",
     ['gd'] = '<cmd>lua vim.lsp.buf.declaration()<CR>',
