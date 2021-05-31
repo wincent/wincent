c146fd5efc911f3c2282b388e52695abf795a309
Greg Hurrell <greg@hurrell.net>
2021-05-31 13:13:19 +0200

    chore(dotfiles): note date for new work machine in .irl.rb
    Haven't set the machine up yet; will do that tomorrow (setting up a
    ~/.zsh/host/ entry and so on to complete this set-up).

--- aspects/dotfiles/files/.zsh/bin/.irl.rb
+++ aspects/dotfiles/files/.zsh/bin/.irl.rb
@@ -3,6 +3,7 @@
 require 'date'
 
 # 2017-10-31: Personal MacBook Pro (Haswell, Mid-2015, dual graphics).
+# 2021-05-31: GitHub MacBook Pro (13-Inch "M1" 8-Core 3.2 (2020)).
 
 year = (ARGV[0] || 2017).to_i
 month = (ARGV[1] || 10).to_i
