fb5f88f96458a250dd8724151acc2b927bf8e3d2
Greg Hurrell <greg@hurrell.net>
2021-06-01 22:55:56 +0200

    feat(interception): set up udev rules
    As noted in the comment, having this in here is a bit of an abuse, but
    the alternatives suck in different ways too:
    
    - Putting it in the "dotfiles" aspect: now we would need `sudo` to
      install dotfiles, and `udev` has nothing to do with dotfiles, really.
    - Making a separate "udev" aspect: lots of overhead for a little thing;
      also, it relies on scripts installed in our home directory... maybe we
      should think about installing elsewhere, but still, pretending that
      this will ever be used by more than just `glh` is a joke.
    
    So, here it goes. Note that I had to adjust the TS config because `tsc`
    was choking on ".ts" (timestamp) files
    
        aspects/interception/support/CMakeFiles/mac2linux.dir/compiler_depend.ts:1:1 - error TS18016: Private identifiers are not allowed outside class bodies.
    
        1 # CMAKE generated file: DO NOT EDIT!
    
        (etc)

--- aspects/interception/index.ts
+++ aspects/interception/index.ts
@@ -62,8 +62,27 @@ task('create /etc/interception/udevmon.yaml', async () => {
   });
 });
 
+// This being in here is semi-sketchy because it isn't strictly related to
+// Interception Tools, but it _is_ related to the keyboard and udev, so we
+// put it here. Depends on scripts installed by the dotfiles aspect, so the
+// separation of concerns is unclear.
+task('create /etc/udev/rules.d/50-realforce-layout.rules', async () => {
+  await template({
+    notify: 'reload udevadm',
+    path: '/etc/udev/rules.d/50-realforce-layout.rules',
+    src: resource.template('50-realforce-layout.rules.erb'),
+    sudo: true,
+  });
+});
+
 handler('enable udevmon', async () => {
   await command('systemctl', ['enable', 'udevmon', '--now'], {
+    sudo: true,
+  });
+});
+
+handler('reload udevadm', async () => {
+  await command('udevadm', ['control', '--reload'], {
     sudo: true,
   });
 });
