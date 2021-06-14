e249db130ae8882add64cc1ae456a69f25e8e8fe
Greg Hurrell <greg@hurrell.net>
2021-06-14 18:36:29 +0200

    feat: install Vim config on Codespaces

--- aspects/apt/aspect.ts
+++ aspects/apt/aspect.ts
@@ -1,6 +1,6 @@
 export default {
   description: 'Installs and updates packages via the apt-get package manager',
   variables: {
-    packages: ['neovim', 'ripgrep'],
+    packages: ['neovim', 'ripgrep', 'ruby-dev'],
   },
 };
