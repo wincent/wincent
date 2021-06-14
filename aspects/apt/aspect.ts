9bb9bdd7fc1e17ce6b5d04330af65749512dbea0
Greg Hurrell <greg@hurrell.net>
2021-06-14 20:36:03 +0200

    chore(apt): add some deps needed to build Neovim from source
    In practice, most of these were already on the system (I only needed to
    manually install "libtool-bin" and "ninja-build", I think), but seeing
    as the install is idempotent, seems like a good idea to enumerate the
    full list. From:
    
        https://github.com/neovim/neovim/wiki/Building-Neovim#ubuntu--debian

--- aspects/apt/aspect.ts
+++ aspects/apt/aspect.ts
@@ -1,6 +1,20 @@
 export default {
   description: 'Installs and updates packages via the apt-get package manager',
   variables: {
-    packages: ['neovim', 'ripgrep', 'ruby-dev'],
+    packages: [
+      'autoconf', // To build Neovim from source.
+      'automake', // To build Neovim from source.
+      'cmake', // To build Neovim from source.
+      'g++', // To build Neovim from source.
+      'gettext', // To build Neovim from source.
+      'libtool', // To build Neovim from source.
+      'libtool-bin', // To build Neovim from source.
+      'neovim', // Editor.
+      'ninja-build', // To build Neovim from source.
+      'pkg-config', // To build Neovim from source.
+      'ripgrep', // Fast file finder.
+      'ruby-dev', // To build Command-T.
+      'unzip', // To build Neovim from source.
+    ],
   },
 };
