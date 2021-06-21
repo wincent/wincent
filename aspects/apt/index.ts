4d867d6d2f13bf88f1a3bf94e37d9de5aee658f1
Greg Hurrell <greg@hurrell.net>
2021-06-21 13:37:00 +0200

    fix(apt): add missing `failedWhen` clause
    Had this in a buffer but had forgotten to write it; this one really does
    work (already tested it).

--- aspects/apt/index.ts
+++ aspects/apt/index.ts
@@ -19,11 +19,11 @@ function task(name: string, callback: () => Promise<void>) {
 
 task('install packages', async () => {
   for (const pkg of variable.strings('packages')) {
-    const result = await command('dpkg-query', [
-      '--show',
-      '--showformat=${db:Status-Status}',
-      pkg,
-    ]);
+    const result = await command(
+      'dpkg-query',
+      ['--show', '--showformat=${db:Status-Status}', pkg],
+      {failedWhen: () => false}
+    );
 
     if (result?.status !== 0 || result?.stdout.includes('not-installed')) {
       await command('apt-get', ['install', '-y', pkg]);
