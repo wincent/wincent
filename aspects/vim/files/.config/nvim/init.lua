f14ab09e676ee036932291bc47121359632d3b50
Greg Hurrell <greg@hurrell.net>
2021-07-08 22:42:18 +0200

    perf(vim): avoid repeated vim.fn.expand() calls
    Nano-optimization with literally no measurable effect. 😂

--- aspects/vim/files/.config/nvim/init.lua
+++ aspects/vim/files/.config/nvim/init.lua
@@ -3,47 +3,48 @@
 -------------------------------------------------------------------------------
 
 local home = vim.env.HOME
+local config = home .. '/.config/nvim'
 local root = vim.env.USER == 'root'
 local vi = vim.v.progname == 'vi'
 
-vim.opt.autoindent     = true                                     -- maintain indent of current line
-vim.opt.backspace      = 'indent,start,eol'                       -- allow unrestricted backspacing in insert mode
-vim.opt.backup         = false                                    -- don't make backups before writing
-vim.opt.backupcopy     = 'yes'                                    -- overwrite files to update, instead of renaming + rewriting
-vim.opt.backupdir      = vim.fn.expand('~/.config/nvim/backup//') -- keep backup files out of the way (ie. if 'backup' is ever set)
-vim.opt.backupdir      = vim.opt.backupdir + '.'                  -- fallback
-vim.opt.backupskip     = vim.opt.backupskip + '*.re,*.rei'        -- prevent bsb's watch mode from getting confused (if 'backup' is ever set)
-vim.opt.belloff        = 'all'                                    -- never ring the bell for any reason
-vim.opt.completeopt    = 'menuone'                                -- show menu even if there is only one candidate (for nvim-compe)
-vim.opt.completeopt    = vim.opt.completeopt + 'noselect'         -- don't automatically select canditate (for nvim-compe)
-vim.opt.cursorline     = true                                     -- highlight current line
-vim.opt.diffopt        = vim.opt.diffopt + 'foldcolumn:0'         -- don't show fold column in diff view
-vim.opt.directory      = vim.fn.expand('~/.config/nvim/swap//')   -- keep swap files out of the way
-vim.opt.directory      = vim.opt.directory + '.'                  -- fallback
-vim.opt.emoji          = false                                    -- don't assume all emoji are double width
-vim.opt.expandtab      = true                                     -- always use spaces instead of tabs
-vim.opt.fillchars      = 'diff:∙'                                 -- BULLET OPERATOR (U+2219, UTF-8: E2 88 99)
-vim.opt.fillchars      = vim.opt.fillchars + 'eob: '              -- NO-BREAK SPACE (U+00A0, UTF-8: C2 A0) to suppress ~ at EndOfBuffer
-vim.opt.fillchars      = vim.opt.fillchars + 'fold:·'             -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
-vim.opt.fillchars      = vim.opt.fillchars + 'vert:┃'             -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
-vim.opt.foldlevelstart = 99                                       -- start unfolded
-vim.opt.foldmethod     = 'indent'                                 -- not as cool as syntax, but faster
+vim.opt.autoindent     = true                              -- maintain indent of current line
+vim.opt.backspace      = 'indent,start,eol'                -- allow unrestricted backspacing in insert mode
+vim.opt.backup         = false                             -- don't make backups before writing
+vim.opt.backupcopy     = 'yes'                             -- overwrite files to update, instead of renaming + rewriting
+vim.opt.backupdir      = config .. '/backup//'             -- keep backup files out of the way (ie. if 'backup' is ever set)
+vim.opt.backupdir      = vim.opt.backupdir + '.'           -- fallback
+vim.opt.backupskip     = vim.opt.backupskip + '*.re,*.rei' -- prevent bsb's watch mode from getting confused (if 'backup' is ever set)
+vim.opt.belloff        = 'all'                             -- never ring the bell for any reason
+vim.opt.completeopt    = 'menuone'                         -- show menu even if there is only one candidate (for nvim-compe)
+vim.opt.completeopt    = vim.opt.completeopt + 'noselect'  -- don't automatically select canditate (for nvim-compe)
+vim.opt.cursorline     = true                              -- highlight current line
+vim.opt.diffopt        = vim.opt.diffopt + 'foldcolumn:0'  -- don't show fold column in diff view
+vim.opt.directory      = config .. '/nvim/swap//'          -- keep swap files out of the way
+vim.opt.directory      = vim.opt.directory + '.'           -- fallback
+vim.opt.emoji          = false                             -- don't assume all emoji are double width
+vim.opt.expandtab      = true                              -- always use spaces instead of tabs
+vim.opt.fillchars      = 'diff:∙'                          -- BULLET OPERATOR (U+2219, UTF-8: E2 88 99)
+vim.opt.fillchars      = vim.opt.fillchars + 'eob: '       -- NO-BREAK SPACE (U+00A0, UTF-8: C2 A0) to suppress ~ at EndOfBuffer
+vim.opt.fillchars      = vim.opt.fillchars + 'fold:·'      -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
+vim.opt.fillchars      = vim.opt.fillchars + 'vert:┃'      -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
+vim.opt.foldlevelstart = 99                                -- start unfolded
+vim.opt.foldmethod     = 'indent'                          -- not as cool as syntax, but faster
 vim.opt.foldtext       = 'wincent#settings#foldtext()'
-vim.opt.formatoptions  = vim.opt.formatoptions + 'j'              -- remove comment leader when joining comment lines
-vim.opt.formatoptions  = vim.opt.formatoptions + 'n'              -- smart auto-indenting inside numbered lists
+vim.opt.formatoptions  = vim.opt.formatoptions + 'j'       -- remove comment leader when joining comment lines
+vim.opt.formatoptions  = vim.opt.formatoptions + 'n'       -- smart auto-indenting inside numbered lists
 vim.opt.guifont        = 'Source Code Pro Light:h13'
-vim.opt.hidden         = true                                     -- allows you to hide buffers with unsaved changes without being prompted
-vim.opt.inccommand     = 'split'                                  -- live preview of :s results
-vim.opt.joinspaces     = false                                    -- don't autoinsert two spaces after '.', '?', '!' for join command
-vim.opt.laststatus     = 2                                        -- always show status line
-vim.opt.lazyredraw     = true                                     -- don't bother updating screen during macro playback
-vim.opt.linebreak      = true                                     -- wrap long lines at characters in 'breakat'
-vim.opt.list           = true                                     -- show whitespace
-vim.opt.listchars      = 'nbsp:⦸'                                 -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
-vim.opt.listchars      = vim.opt.listchars + 'extends:»'          -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
-vim.opt.listchars      = vim.opt.listchars + 'precedes:«'         -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
-vim.opt.listchars      = vim.opt.listchars + 'tab:▷┅'             -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + BOX DRAWINGS HEAVY TRIPLE DASH HORIZONTAL (U+2505, UTF-8: E2 94 85)
-vim.opt.listchars      = vim.opt.listchars + 'trail:•'            -- BULLET (U+2022, UTF-8: E2 80 A2)
+vim.opt.hidden         = true                              -- allows you to hide buffers with unsaved changes without being prompted
+vim.opt.inccommand     = 'split'                           -- live preview of :s results
+vim.opt.joinspaces     = false                             -- don't autoinsert two spaces after '.', '?', '!' for join command
+vim.opt.laststatus     = 2                                 -- always show status line
+vim.opt.lazyredraw     = true                              -- don't bother updating screen during macro playback
+vim.opt.linebreak      = true                              -- wrap long lines at characters in 'breakat'
+vim.opt.list           = true                              -- show whitespace
+vim.opt.listchars      = 'nbsp:⦸'                          -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
+vim.opt.listchars      = vim.opt.listchars + 'extends:»'   -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
+vim.opt.listchars      = vim.opt.listchars + 'precedes:«'  -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
+vim.opt.listchars      = vim.opt.listchars + 'tab:▷┅'      -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + BOX DRAWINGS HEAVY TRIPLE DASH HORIZONTAL (U+2505, UTF-8: E2 94 85)
+vim.opt.listchars      = vim.opt.listchars + 'trail:•'     -- BULLET (U+2022, UTF-8: E2 80 A2)
 
 if vi then
   vim.opt.loadplugins = false
@@ -113,14 +114,14 @@ vim.opt.textwidth     = 80                      -- automatically hard wrap at 80
 if root then
   vim.opt.undofile = false -- don't create root-owned files
 else
-  vim.opt.undodir  = vim.fn.expand('~/.config/nvim/undo//') -- keep undo files out of the way
-  vim.opt.undodir  = vim.opt.undodir + '.'                  -- fallback
-  vim.opt.undofile = true                                   -- actually use undo files
+  vim.opt.undodir  = config .. '/undo//'   -- keep undo files out of the way
+  vim.opt.undodir  = vim.opt.undodir + '.' -- fallback
+  vim.opt.undofile = true                  -- actually use undo files
 end
 
 vim.opt.updatecount = 80                                    -- update swapfiles every 80 typed chars
 vim.opt.updatetime  = 2000                                  -- CursorHold interval
-vim.opt.viewdir     = vim.fn.expand('~/.config/nvim/view')  -- where to store files for :mkview
+vim.opt.viewdir     = config .. '/view'                     -- where to store files for :mkview
 vim.opt.viewoptions = 'cursor,folds'                        -- save/restore just these (with `:{mk,load}view`)
 vim.opt.virtualedit = 'block'                               -- allow cursor to move where there is no text in visual block mode
 vim.opt.visualbell  = true                                  -- stop annoying beeping for non-error errors
