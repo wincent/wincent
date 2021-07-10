2d2c9ac7cf80e87b576810ff1b284aeb41b6d1ea
Greg Hurrell <greg@hurrell.net>
2021-07-10 23:41:52 +0200

    feat(nvim): add bindings to resize windows
    I have keyboard bindings to resize tmux splits and I use them all the
    time. I find it sorely lacking when I can't do the same in Nvim.
    
    - Because screens are 16:9 or similar nowadays, the most common resize I
      need is horizontal, so I have that assigned to <Leader> (easier to
      reach than <LocalLeader>).
    
    - Mnemonic here is "+"/"-", but given that you only get "+" by holding
      down shift, bindings are actually to "="/"-" respectively (because
      unshifted "+" is actually "=").
    
    - Vim is weird. Quoting the help:
    
          :vert[ical] res[ize] [N]                    *:vertical-resize* *CTRL-W_bar*
          CTRL-W |          Set current window width to N (default: widest possible).
    
      ie. to resize _horizontally_ (that is, to change the _width_), you use
      the `:vertical resize` command. I sure hope there was some cognitive
      dissonance going on when this feature was implemented and documented,
      because if not, I can't explain it. 😂
    
      Tmux uses the opposite terminology. `split-window -h` causes windows
      to be split horizontally, which means a vertical separator appears,
      creating a side-by-side configuration. As `:h :vertical` says, Vim
      uses the terminology in the opposite way:
    
          Execute {cmd}.  If it contains a command that splits a window,
          it will be split vertically.
    
      If you run `:vertical help help`, it will _also_ create a vertical
      separator and two side-by-side windows.
    
    One final thing to note: I haven't ported the other "leader.vim"
    mappings to Lua yet, so for now the two files exist side-by-side, just
    like my split windows...

---
+++ aspects/nvim/files/.config/nvim/plugin/mappings/leader.lua
@@ -0,0 +1,11 @@
+local wincent = require'wincent'
+
+local nnoremap = wincent.vim.nnoremap
+
+-- Grow/shrink window horizontally (ie. make wider or narrower).
+nnoremap('<Leader>=', ':vertical resize +5<CR>', {silent = true})
+nnoremap('<Leader>-', ':vertical resize -5<CR>', {silent = true})
+
+-- Grow/shrink window vertically (ie. make taller or shorter).
+nnoremap('<LocalLeader>=', ':resize +5<CR>', {silent = true})
+nnoremap('<LocalLeader>-', ':resize -5<CR>', {silent = true})
