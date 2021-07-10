ebe48dcca3266a170d9dcb5464a000b3b35ce3e9
Greg Hurrell <greg@hurrell.net>
2021-07-10 12:40:27 +0200

    refactor(vim): teach `using()` to load multiple modules
    This implements the idea mentioned in the parent commit. But I still
    don't like it...

--- aspects/vim/files/.config/nvim/lua/wincent/util/using.lua
+++ aspects/vim/files/.config/nvim/lua/wincent/util/using.lua
@@ -1,14 +1,21 @@
 -- Pattern for safely using a module that may not be present.
 --
---    using('compe', function (compe)
---      print('only executed if compe is available')
+--    using('compe', 'luasnip', function (compe, luasnip)
+--      print('only executed if compe and luasnip are available')
 --    end)
-local using = function(name, callback)
-  local has_module, required = pcall(require, name)
-
-  if has_module then
-    return callback(required)
+local using = function(...)
+  local names = {...}
+  local callback = table.remove(names)
+  local modules = {}
+  for _, name in ipairs(names) do
+    local has_module, required = pcall(require, name)
+    if has_module then
+      table.insert(modules, required)
+    else
+      return
+    end
   end
+  callback(unpack(modules))
 end
 
 return using
