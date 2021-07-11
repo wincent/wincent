cdd730daefd0b7f2e4caf2d2117b3b2caccac987
Greg Hurrell <greg@hurrell.net>
2021-07-11 22:40:14 +0200

    refactor(nvim): use explicit global `_G` table explicitly
    As recommended here:
    
        https://twitter.com/ahmedelgabri/status/1414310728396312580
    
    `_G` is good because it is clearly an intentional assignment to the
    global table. In contrast, this one:
    
        wincent = something
    
    has two problems:
    
    1. The Sumneko Lua LSP server is going to complain about a global that
       starts with a lowercase letter.
    2. It might look like a mistake (a forgotten `local`).

--- aspects/nvim/files/.config/nvim/lua/wincent/g.lua
+++ aspects/nvim/files/.config/nvim/lua/wincent/g.lua
@@ -1,16 +1,12 @@
 -- Using a real global here to make sure anything stashed in here survives even
 -- after the last reference to it goes away.
 --
--- We use a lowercase variable name — even though the LSP doesn't like that
--- (it says "Global variable in lowercase initial") — to facilitate access via
--- `v:lua.wincent.g`.
---
 -- But do note: if you use the global in this way, you should _still_
 -- `require'wincent.g'` (or `require'wincent'` and then access
 -- `wincent.g`), to ensure that the globals are initialized before any
 -- access via `v:lua`.
 --
-wincent = {
+_G.wincent = {
   g = {},
 }
 
