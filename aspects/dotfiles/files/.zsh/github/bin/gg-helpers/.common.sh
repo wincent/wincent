eb97505e8f29025ac66c64ee62b4a0427fffa7f0
Greg Hurrell <greg@hurrell.net>
2021-07-12 16:47:54 +0200

    chore(dotfiles): resurrect base `portool` structure from history
    Because I am going to use the same pattern to produce a helper tool for
    common tasks at work. Unfortunately, I don't have such an obviously
    awesome name this time. so going with `gg` which isn't short for
    anything at all but is GitHub-ish.

---
+++ aspects/dotfiles/files/.zsh/github/bin/gg-helpers/.common.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+PORTOOL_BASE="${BASH_SOURCE%/*}"
+
+TOMCAT=tomcat-9.0.43
+
+if [[ $PWD = $HOME/code/portal-master* ]]; then
+  PORTAL_DB=lportal2_master
+  PORTAL_CONTEXT=$HOME/code/portal-master
+  PORTAL_ROOT=$PORTAL_CONTEXT/liferay-portal
+elif [[ $PWD = $HOME/code/portal-ee* ]]; then
+  PORTAL_DB=
+  PORTAL_CONTEXT=$HOME/code/portal-ee
+  PORTAL_ROOT=$PORTAL_CONTEXT/liferay-portal-ee
+elif [[ $PWD = $HOME/code/portal* ]]; then
+  PORTAL_DB=lportal_master
+  PORTAL_CONTEXT=$HOME/code/portal
+  PORTAL_ROOT=$PORTAL_CONTEXT/liferay-portal
+else
+  PORTAL_FALLBACK=1
+  PORTAL_DB=lportal_master
+  PORTAL_CONTEXT=$HOME/code/portal
+  PORTAL_ROOT=$PORTAL_CONTEXT/liferay-portal
+fi
+
+PORTAL_BUNDLES=$PORTAL_CONTEXT/bundles
+
+# Based on: https://stackoverflow.com/a/8351489/2103996
+backoff() {
+  local max_attempts=${ATTEMPTS-5}
+  local timeout=${TIMEOUT-1}
+  local attempt=1
+  local exitCode=0
+
+  while (( $attempt < $max_attempts ))
+  do
+    if "$@"
+    then
+      return 0
+    else
+      exitCode=$?
+    fi
+
+    echo "error: retrying in $timeout..." 1>&2
+    sleep $timeout
+    attempt=$(( attempt + 1 ))
+    timeout=$(( timeout * 2 ))
+  done
+
+  if [[ $exitCode != 0 ]]
+  then
+    echo "error: maximum attempt count exceeded ($@)" 1>&2
+  fi
+
+  return $exitCode
+}
+
+node_path() {
+  local DIR=$PWD
+
+  local NODE_BINDIR="$HOME/code/portal/liferay-portal/build/node/bin"
+
+  # File to use to find nearest liferay-portal checkout.
+  local SENTINEL=release.properties
+
+  while [ -n "$DIR" ]; do
+    if [ -e "$DIR/$SENTINEL" ]; then
+      local EXECUTABLE="$DIR/build/node/bin/node"
+
+      if [ -x "$EXECUTABLE" ]; then
+        echo "info: using $EXECUTABLE" >&2
+
+        NODE_BINDIR="$DIR/build/node/bin"
+      fi
+
+      break
+    else
+      DIR=${DIR%/*}
+    fi
+  done
+
+  local NODE_BINARY="$NODE_BINDIR/node"
+
+  if [ ! -x "$NODE_BINARY" ]; then
+    echo "error: no $NODE_BINARY executable" >&2
+    exit 1
+  fi
+
+  echo $NODE_BINDIR
+}
+
+signal_proc() {
+  SIG=$1
+  TARGET=$2
+
+  PIDS=$(pgrep -f "$TARGET")
+
+  if [ -z "$PIDS" ]; then
+    echo "error: \`$TARGET\` is not running - nothing to send $SIG to"
+    exit 1
+  fi
+
+  for PID in $PIDS; do
+    GROUP=$(ps -p "$PID" -o pgid | sed -e 1d -e 's/ *//')
+    if [ -z "$GROUP" ]; then
+      echo "error: unable to determine process group of process $PID"
+      exit 1
+    fi
+    echo "Sending $SIG to process group $GROUP of process $PID"
+    kill "-$SIG" "-$GROUP"
+  done
+}
