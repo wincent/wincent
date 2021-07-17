93d1098886f08e7512afacadcd5e830c5e8a2d2b
Greg Hurrell <greg@hurrell.net>
2021-07-17 22:36:03 +0200

    feat(nvim): improve introspectability of anonymous Lua callbacks
    Starting with mappings, but I'll apply this to the other places we use
    anonymous functions.
    
    Before, running `:nmap j` would print:
    
    ```
    n  j           * v:lua.wincent.g.map_callbacks._13()
    ```
    
    Now, it prints:
    
    ```
    n  j           * v:lua.wincent.g.map_callbacks.plugin_mappings_normal_L38()
    ```
    
    The callback key is produced by grabbing the path to the function via
    `debug.getinfo`, stripping off the prefix (eg. `~/.config/nvim`),
    dropping the extension, turning non-alphanumeric characters (basically
    that means slashes) into underscores, appending the line-number, and if
    necessary to resolve collisions, appending a monotonically increasing
    index number (this can happen if the same anonymous function is added
    repeatedly).
    
    Closes: https://github.com/wincent/wincent/issues/115

--- aspects/nvim/files/.config/nvim/lua/wincent/vim/map.lua
+++ aspects/nvim/files/.config/nvim/lua/wincent/vim/map.lua
@@ -1,17 +1,38 @@
 wincent.g.map_callbacks = {}
 
+-- Used as tie-breaker in event that multiple registrations happen for same file
+-- + line.
 local callback_index = 0
+
+local config_prefix = vim.env.HOME .. '/.config/nvim/'
 
 -- TODO: For completeness, should have unmap() too, and other variants
 -- as they arise (nunmap() etc); but for now just going with a "dispose"
 -- function as return value.
 
+local get_key = function (fn)
+  local info = debug.getinfo(fn)
+  local key = info.short_src
+  if vim.startswith(key, config_prefix) then
+    key = key:sub(#config_prefix + 1)
+  end
+  if vim.endswith(key, '.lua') then -- and sure would be weird if it _didn't_
+    key = key:sub(1, #key - 4)
+  end
+  key = key:gsub('%W', '_')
+  key = key .. '_L' .. info.linedefined
+  if wincent.g.map_callbacks[key] ~= nil then
+    key = key .. '_' .. callback_index
+    callback_index = callback_index + 1
+  end
+  return key
+end
+
 local map = function (mode, lhs, rhs, opts)
   opts = opts or {}
   local rhs_type = type(rhs)
   if rhs_type == 'function' then
-    local key = '_' .. callback_index
-    callback_index = callback_index + 1
+    local key = get_key(rhs)
     wincent.g.map_callbacks[key] = rhs
     rhs = 'v:lua.wincent.g.map_callbacks.' .. key .. '()'
   elseif rhs_type ~= 'string' then
