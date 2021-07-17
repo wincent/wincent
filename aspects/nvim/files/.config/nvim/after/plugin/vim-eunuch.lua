f7eb5ced2a27e7f79249d30c0de981f485d4a01d
Greg Hurrell <greg@hurrell.net>
2021-07-17 12:31:21 +0200

    feat(nvim): make :W work in Neovim
    In porting vim-eunuch.vim to vim-eunuch.lua, I was reminded that the old
    trick doesn't work in Neovim. I had barely noticed because the truth is
    that I had trained myself to _not_ open root-owned files for writing
    from an unprivileged user.
    
    But anyway, we can fix it. I'm doing this "inside" Vim because I want to
    have a way of getting the password to `sudo` without having to find a
    variant of `SUDO_ASKPASS` that works for each platform I use.
    
    `:W` prompts for password, writes the file, caches the pass for 5
    minutes.
    
    `:W!` will prompt you for a password even if one is already cached
    (useful if you entered a wrong pass and it got cached).
    
    Security disclaimer: this asks for your password and stores it in a
    variable for 5 minutes. Even after the variable contents are
    garbage-collected, the text of the password may be recoverable from
    memory using various tricks (debuggers etc). The password is
    additionally (briefly) written to disk, where it is only as secure as
    filesystem permissions allow (eg. does your OS return a truly private
    `tempname()`? is the 700 mode adequately enforced on your filesystem?
    are there other privileged users running on the system that would try to
    read the pass? once the file is unlinked, when do the actual contents
    get truly purged from storage? etc).
    
    As I use this rarely, I am fine with all these trade-offs. But your
    mileage may vary.
    
    The other gotcha is that if you enter a wrong pass, Vim will pause while
    `sudo` asks two more times for the (same) wrong password. At this point
    if you want to retry you should use `:W!`. Again, I'm fine with this
    trade-off because I don't think there is an easy workaround.

---
+++ aspects/nvim/files/.config/nvim/after/plugin/vim-eunuch.lua
@@ -0,0 +1,17 @@
+-- vim-eunuch defines a ":W" command that I consider a little dangerous
+-- (too easy to type, with possibly wide-reaching side-effects); overwrite
+-- it with something useful (ie. for writing files with root privileges).
+--
+--    :W        writes the current file with root privileges, prompting for a
+--              password if one isn't already cached; passwords are cached for
+--              5 minutes
+--
+--    :W!       as above, but always prompts for a password
+--
+-- Note that we can't do the naive that worked in Vim:
+--
+--     vim.cmd('command! W w !sudo tee % > /dev/null')
+--
+-- Due to: https://github.com/neovim/neovim/issues/1716
+
+vim.cmd('command! -bang W call v:lua.wincent.sudo.write("<bang>")')
---
+++ aspects/nvim/files/.config/nvim/lua/wincent/sudo/write.lua
@@ -0,0 +1,43 @@
+local cache_time = 5 -- * 60 * 1000 -- 5 minutes
+local password = nil
+local timer = nil
+
+local expect = function(expected, fn, description)
+  local result = fn()
+  if result ~= expected then
+    error('error: wincent.sudo.write got result ' .. result .. ' for ' .. description .. '(expected ' .. expected .. ')')
+  end
+end
+
+local reject = function(rejected, fn, description)
+  local result = fn()
+  if result == rejected then
+    error('error: wincent.sudo.write got result ' .. result .. ' for ' .. description .. '(expected non-' .. rejected .. ')')
+  end
+end
+
+local write = function(bang)
+  if bang == '!' or password == nil then
+    password = vim.fn.inputsecret('Password:')
+    if timer ~= nil then
+      timer:stop()
+    end
+    timer = vim.defer_fn(function()
+      password = nil
+    end, cache_time)
+  end
+
+  local askpass = vim.fn.tempname()
+
+  expect(0, function() return vim.fn.writefile({''}, askpass, 's') end, 'writefile (touch)')
+  reject(0, function() return vim.fn.setfperm(askpass, 'rwx------') end, 'setfperm')
+  expect(0, function() return vim.fn.writefile({'#!/bin/bash', '/bin/echo -n ' .. vim.fn.shellescape(password)}, askpass, 's') end, 'writefile (fill)')
+
+  pcall(function()
+    vim.cmd('silent write !env SUDO_ASKPASS=' .. vim.fn.shellescape(askpass) .. '  sudo -A tee % > /dev/null')
+  end)
+
+  expect(0, function() return vim.fn.delete(askpass) end, 'delete')
+end
+
+return write
