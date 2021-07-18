4b4e8725e402d91125aac4c66bdbecf74505f5f7
Greg Hurrell <greg@hurrell.net>
2021-07-18 23:42:46 +0200

    refactor(nvim): switch tab listchar for benefit of kitty
    Works around:
    
        https://github.com/kovidgoyal/kitty/issues/3844
    
    by picking a new glyph that looks almost the same, but which renders
    adequately on both iTerm and kitty.

--- aspects/nvim/files/.config/nvim/init.lua
+++ aspects/nvim/files/.config/nvim/init.lua
@@ -48,7 +48,7 @@ vim.opt.listchars      = {
   nbsp                 = '⦸',                              -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
   extends              = '»',                              -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
   precedes             = '«',                              -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
-  tab                  = '▷┅',                             -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + BOX DRAWINGS HEAVY TRIPLE DASH HORIZONTAL (U+2505, UTF-8: E2 94 85)
+  tab                  = '▷⋯',                             -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
   trail                = '•',                              -- BULLET (U+2022, UTF-8: E2 80 A2)
 }
 
