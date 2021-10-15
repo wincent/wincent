e06c9fae1f548f633f7be635cff4ca53b8d63f7b
Greg Hurrell <greg@hurrell.net>
2021-10-15 18:52:18 +0200

    refactor(dotfiles): fold "git zap" and "git zip" into one script, "git sync"
    As noted previously, I always run these two together, so it makes sense
    for them to be melded into one script.
    
    Fun fact: before I started this, I thought I was going to need an
    associative array, so I switched over to Zsh. In the end, I didn't need
    it, but I left it in here to: (a) show my work, and (b) have an example
    of how to do some useful things in Zsh, like find out the path of the
    currently script (like you can in Bash with `$BASH_SOURCE`).
    
    In the next commit, I'll delete the unneeded/unused associative array,
    but I'm keeping the script in Zsh for now.

---
+++ aspects/dotfiles/files/.zsh/bin/.common.zsh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+BOLD=$(tput bold)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+RESET=$(tput sgr0)
+YELLOW=$(tput setaf 3)
+
+bold() {
+  echo -n "${BOLD}$1${RESET}"
+}
+
+green() {
+  echo -n "${GREEN}${BOLD}$1${RESET}"
+}
+
+red() {
+  echo -n "${RED}${BOLD}$1${RESET}"
+}
+
+yellow() {
+  echo -n "${YELLOW}${BOLD}$1${RESET}"
+}
