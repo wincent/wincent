5e380622326c0c9131dbc07cbab15d7f1a304488
Greg Hurrell <greg@hurrell.net>
2021-10-29 16:47:24 +0200

    feat(dotfiles): add Raycast script command for jumping to pulls

---
+++ aspects/dotfiles/files/.config/Raycast/Script Commands/github-pulls.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title GitHub pulls
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ./github.png
+# @raycast.argument1 { "type": "text", "optional": true, "placeholder": "repo", "percentEncoded": false }
+
+# Documentation:
+# @raycast.description Jump to a specified GitHub repo's pull requests
+
+# Get path to current script's directory:
+# https://unix.stackexchange.com/a/115431/140622
+BASE_DIRECTORY=${0:a:h}
+source "$BASE_DIRECTORY/.common.zsh"
+
+REPO=$(trim "$*")
+
+if [ -z "$REPO" ]; then
+  open "https://github.com/github/github/pulls"
+elif [[ $REPO != */* ]]; then
+  REPO=$(percent_encode "$REPO")
+  open "https://github.com/github/${REPO}/pulls"
+else
+  OWNER=$(dirname "$REPO")
+  if [ "$OWNER" != . ]; then
+    OWNER=$(percent_encode "$OWNER")
+    REPO=$(basename "$REPO")
+    REPO=$(percent_encode "$REPO")
+    open "https://github.com/${OWNER}/${REPO}/pulls"
+  else
+    REPO=$(percent_encode "$REPO")
+    open "https://github.com/${REPO}/pulls"
+  fi
+fi
