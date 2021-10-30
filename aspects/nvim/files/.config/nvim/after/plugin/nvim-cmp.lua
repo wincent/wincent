c7edb6d6902d053af4d9e3928cfce281b7e625a6
Greg Hurrell <greg@hurrell.net>
2021-10-30 22:20:18 +0200

    fix(nvim): rework autocomplete mappings to work properly again
    Because things were semi-broken in the recent move from nvim-compe to
    nvim-cmp. I think this is mostly right at this point.

--- aspects/nvim/files/.config/nvim/after/plugin/nvim-cmp.lua
+++ aspects/nvim/files/.config/nvim/after/plugin/nvim-cmp.lua
@@ -1,9 +1,170 @@
 local has_cmp, cmp = pcall(require, 'cmp')
+
 if has_cmp then
+  local rhs = wincent.vim.rhs
+  local has_luasnip, luasnip = pcall(require, 'luasnip')
+
+  -- Returns the current column number.
+  local column = function()
+    local _line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col
+  end
+
+  -- Returns true if the cursor is in leftmost column or at a whitespace
+  -- character.
+  local in_whitespace = function()
+    local col = column()
+    return col == 0 or vim.api.nvim_get_current_line():sub(col, col):match('%s')
+  end
+
+  local shift_width = function()
+    if vim.o.softtabstop <= 0 then
+      return vim.fn.shiftwidth()
+    else
+      return vim.o.softtabstop
+    end
+  end
+
+  -- Complement to `smart_tab()`.
+  --
+  -- When 'noexpandtab' is set (ie. hard tabs are in use), backspace:
+  --
+  --    - On the left (ie. in the indent) will delete a tab.
+  --    - On the right (when in trailing whitespace) will delete enough
+  --      spaces to get back to the previous tabstop.
+  --    - Everywhere else it will just delete the previous character.
+  --
+  -- For other buffers ('expandtab'), we let Neovim behave as standard and that
+  -- yields intuitive behavior.
+  local smart_bs = function ()
+    if vim.o.expandtab then
+      return rhs('<BS>')
+    else
+      local col = column()
+      local line = vim.api.nvim_get_current_line()
+      local prefix = line:sub(1, col)
+      local in_leading_indent = prefix:find('^%s*$')
+      if in_leading_indent then
+        return rhs('<BS>')
+      end
+      local previous_char = prefix:sub(#prefix, #prefix)
+      if previous_char ~= ' ' then
+        return rhs('<BS>')
+      end
+      -- Delete enough spaces to take us back to the previous tabstop.
+      --
+      -- Originally I was calculating the number of <BS> to send, but
+      -- Neovim has some special casing that causes one <BS> to delete
+      -- multiple characters even when 'expandtab' is off (eg. if you hit
+      -- <BS> after pressing <CR> on a line with trailing whitespace and
+      -- Neovim inserts whitespace to match.
+      --
+      -- So, turn 'expandtab' on temporarily and let Neovim figure out
+      -- what a single <BS> should do.
+      --
+      -- See `:h i_CTRL-\_CTRL-O`.
+      return rhs('<C-\\><C-o>:set expandtab<CR><BS><C-\\><C-o>:set noexpandtab<CR>')
+    end
+  end
+
+  -- In buffers where 'noexpandtab' is set (ie. hard tabs are in use), <Tab>:
+  --
+  --    - Inserts a tab on the left (for indentation).
+  --    - Inserts spaces everywhere else (for alignment).
+  --
+  -- For other buffers (ie. where 'expandtab' applies), we use spaces everywhere.
+  local smart_tab = function(opts)
+    local keys = nil
+    if vim.o.expandtab then
+      keys = '<Tab>' -- Neovim will insert spaces.
+    else
+      local col = column()
+      local line = vim.api.nvim_get_current_line()
+      local prefix = line:sub(1, col)
+      local in_leading_indent = prefix:find('^%s*$')
+      if in_leading_indent then
+        keys = '<Tab>' -- Neovim will insert a hard tab.
+      else
+        -- virtcol() returns last column occupied, so if cursor is on a
+        -- tab it will report `actual column + tabstop` instead of `actual
+        -- column`. So, get last column of previous character instead, and
+        -- add 1 to it.
+        local sw = shift_width()
+        local previous_char = prefix:sub(#prefix, #prefix)
+        local previous_column = #prefix - #previous_char + 1
+        local current_column = vim.fn.virtcol({vim.fn.line('.'), previous_column}) + 1
+        local remainder = (current_column - 1) % sw
+        local move = remainder == 0 and sw or sw - remainder
+        keys = (' '):rep(move)
+      end
+    end
+
+    vim.api.nvim_feedkeys(rhs(keys), 'nt', true)
+  end
+
   cmp.setup {
+    mapping = {
+      ['<BS>'] = cmp.mapping(function(_fallback)
+        local keys = smart_bs()
+        vim.api.nvim_feedkeys(keys, 'nt', true)
+      end, { 'i', 's' }),
+
+      ['<C-e>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.close()
+        elseif has_luasnip and lusnip.choice_active() then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+
+      ['<C-j>'] = cmp.get_config().mapping['<Down>'],
+      ['<C-k>'] = cmp.get_config().mapping['<Up>'],
+
+      ['<C-y>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.confirm({ select = true })
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif has_luasnip and luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+
+      ['<Tab>'] = cmp.mapping(function(_fallback)
+        if cmp.visible() then
+          -- Hack alert (abusing private API): if there is only one completion
+          -- candidate, use it.
+          print(#cmp.core.view:_get_entries_view().entries)
+          if #cmp.core.view:_get_entries_view().entries == 1 then
+            cmp.confirm({ select = true })
+          else
+            cmp.select_next_item()
+          end
+        elseif has_luasnip and luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif in_whitespace() then
+          smart_tab()
+        else
+          cmp.complete()
+        end
+      end, { 'i', 's' }),
+    },
+
     snippet = {
       expand = function(args)
-        require('luasnip').lsp_expand(args.body)
+        if has_luasnip then
+          luasnip.lsp_expand(args.body)
+        end
       end,
     },
 
--- aspects/nvim/files/.config/nvim/plugin/snippets.lua
+++ aspects/nvim/files/.config/nvim/plugin/snippets.lua
@@ -10,6 +10,48 @@ if has_luasnip then
 
   luasnip.config.setup({store_selection_keys="<Tab>"})
 
+  -- Snippets common to JS and TS.
+  local js_ts = {
+    -- TODO: make these smart about whether to use trailing semi or
+    -- not, based on directory (or maybe .editorconfig)
+    s(
+      {trig = 'log', dscr = 'console.log'},
+      {t('console.log('), i(1, 'value'), t(');')}
+    ),
+    s(
+      {trig = 'require', dscr = 'require statement'},
+      {t('const '),
+      i(1, 'ModuleName'),
+      t(" = require('"),
+      d(2, function (nodes)
+        return sn(1, {i(1, nodes[1][1])})
+      end, {1}),
+      t("');"),
+    }
+    ),
+    s(
+      {trig = '**', dscr = 'docblock'},
+      {
+        t({'/**', ''}),
+        f(function (args, snip)
+          local lines = vim.tbl_map(
+            function(line)
+              return ' * ' .. vim.trim(line)
+            end,
+            snip.env.SELECT_RAW
+          )
+          if #lines == 0 then
+            return ' * '
+          else
+            return lines
+          end
+        end, {}),
+        i(1),
+        t({'', ' */'}),
+      }
+    ),
+  }
+
   luasnip.snippets = {
     all = {
       s(
@@ -17,47 +59,7 @@ if has_luasnip then
         t('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
       ),
     },
-    javascript = {
-      -- TODO: make these smart about whether to use trailing semi or
-      -- not, based on directory (or maybe .editorconfig)
-      s(
-        {trig = 'log', dscr = 'console.log'},
-        {t('console.log('), i(1, 'value'), t(');')}
-      ),
-      s(
-        {trig = 'require', dscr = 'require statement'},
-        {t('const '),
-        i(1, 'ModuleName'),
-        t(" = require('"),
-        d(2, function (nodes)
-          return sn(1, {i(1, nodes[1][1])})
-        end, {1}),
-        t("');"),
-      }
-      ),
-      s(
-        {trig = '**', dscr = 'docblock'},
-        {
-          t({'/**', ''}),
-          f(function (args)
-            local lines = vim.tbl_map(
-              function(line)
-                print(vim.inspect(line))
-                return ' * ' .. vim.trim(line)
-              end,
-              args[1].env.TM_SELECTED_TEXT
-            )
-            if #lines == 0 then
-              return ' * '
-            else
-              return lines
-            end
-          end, {}),
-          i(1),
-          t({'', ' */'}),
-        }
-      ),
-    },
+    javascript = js_ts,
     markdown = {
       s(
         {trig = 'frontmatter', dscr = 'Document frontmatter'},
@@ -83,5 +85,6 @@ if has_luasnip then
         {t('test "'), i(1, 'description'), t({'" do', '  '}), i(2, '# body'), t({'', 'end'})}
       ),
     },
+    typescript = js_ts,
   }
 end
