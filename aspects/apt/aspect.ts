8c0a8a4931a8cc2fbae20436272ed2303491a26c
Greg Hurrell <greg@hurrell.net>
2022-01-14 16:09:41 +0100

    chore(apt): add `bfs` to the installation list

--- aspects/apt/aspect.ts
+++ aspects/apt/aspect.ts
@@ -4,6 +4,7 @@ export default {
     packages: [
       'autoconf', // To build Neovim from source.
       'automake', // To build Neovim from source.
+      'bfs', // Used in `j` alias.
       'cargo', // To build skim from source.
       'cmake', // To build Neovim from source.
       'g++', // To build Neovim from source.
