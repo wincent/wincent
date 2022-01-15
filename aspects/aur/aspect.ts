4a0d163bdc7f2a6ab115949b2756f655cff8fca6
Greg Hurrell <greg@hurrell.net>
2022-01-15 10:52:41 +0100

    chore(aur): add Edge browser
    Although it is already flagged as out of date... But I want to be able
    to use those sweet vertical tabs.

--- aspects/aur/aspect.ts
+++ aspects/aur/aspect.ts
@@ -9,6 +9,7 @@ export default {
       'it87-dkms-git', // Allows `sensors` to pick up on it8688 chipset.
       'lf', // Command-line file explorer.
       'lua-language-server', // https://github.com/sumneko/lua-language-server
+      'microsoft-edge-stable-bin', // Web browser.
       'obs-studio-git', // Open Broadcaster Software (OBS) for streaming/screen-casting.
       'par', // Paragraph formatter.
       'ttf-symbola', // Font with lots of Unicode glyphs (eg. u276f, for prompt).
