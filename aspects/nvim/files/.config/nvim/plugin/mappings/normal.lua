47984efec93841c3979c4847ace1e592250ce1a4
Greg Hurrell <greg@hurrell.net>
2022-03-04 12:31:45 +0100

    fix(nvim): make indent-blankline.nvim play nicely with fold markers
    Otherwise, it ends up overwriting the left side of the marker. I did
    think about indenting the markers (from my `foldtext` function), but I
    think this is going to look better.

--- aspects/nvim/files/.config/nvim/plugin/mappings/normal.lua
+++ aspects/nvim/files/.config/nvim/plugin/mappings/normal.lua
@@ -5,8 +5,34 @@
 local nnoremap = wincent.vim.nnoremap
 local noremap = wincent.vim.noremap
 
+-- Wrap all mappings related to folding so that indent-blankline.nvim can
+-- update when folds are changed. See:
+-- https://github.com/lukas-reineke/indent-blankline.nvim/issues/118
+local fold = function(mapping)
+  if vim.g.loaded_indent_blankline == 1 then
+    return mapping .. ':IndentBlanklineRefresh<CR>'
+  else
+    return mapping
+  end
+end
+
 -- Toggle fold at current position.
-nnoremap('<Tab>', 'za')
+nnoremap('<Tab>', fold('za'), {silent = true})
+
+-- Other fold-related remaps for compatibility with indent-blankline.nvim:
+nnoremap('zA', fold('zA'), {silent = true})
+nnoremap('zC', fold('zC'), {silent = true})
+nnoremap('zM', fold('zM'), {silent = true})
+nnoremap('zO', fold('zO'), {silent = true})
+nnoremap('zR', fold('zR'), {silent = true})
+nnoremap('zX', fold('zX'), {silent = true})
+nnoremap('za', fold('za'), {silent = true})
+nnoremap('zc', fold('zc'), {silent = true})
+nnoremap('zm', fold('zm'), {silent = true})
+nnoremap('zo', fold('zo'), {silent = true})
+nnoremap('zr', fold('zr'), {silent = true})
+nnoremap('zv', fold('zv'), {silent = true})
+nnoremap('zx', fold('zx'), {silent = true})
 
 -- Relying on Karabiner-Elements (macOS) or Interception Tools (Linux) to avoid
 -- collision between <Tab> and <C-i> (have it send F6 instead for <C-i>).
