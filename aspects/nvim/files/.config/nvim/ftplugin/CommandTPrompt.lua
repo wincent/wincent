3fed25eb7de753b5fa9b61a7dffcdf0c47258239
Greg Hurrell <wincent@github.com>
2022-07-08 17:57:30 +0200

    chore(nvim): stop nvim-cmp from showing popup in CommandTPrompt window
    To accommodate my current playing around with the Lua rewrite on the
    (current) `pu` branch of the Command-T repo. (Disclaimer: `pu` AKA
    "proposed updates" may be rewritten, force-pushed, or deleted at any
    time.)

---
+++ aspects/nvim/files/.config/nvim/ftplugin/CommandTPrompt.lua
@@ -0,0 +1,14 @@
+-- Stop nvim-cmp from showing autocomplete popup while typing into
+-- CommandTPrompt window.
+
+local has_cmp, cmp = pcall(require, 'cmp')
+if not has_cmp then
+  return
+end
+
+if not wincent.g.CommandTPrompt then
+  wincent.g.CommandTPrompt = {registered = true}
+  cmp.setup.filetype('CommandTPrompt', {
+    sources = cmp.config.sources({}),
+  })
+end
