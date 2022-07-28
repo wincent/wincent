ca78c8eb3f58a736fe2cab20cf06dbba5e6207ac
Greg Hurrell <wincent@github.com>
2022-07-28 11:34:35 +0200

    feat: guard against numeric hostname
    No idea why this happened, but on my work Mac, `hostname` in the shell
    and `require('os').hostname()` in NodeJS were both returning a numeric
    IP address. This in turn produced a host handle of "192" which was
    obviously useless, and broke a bunch of stuff (like, for example, I
    couldn't make Git commits!).
    
        https://apple.stackexchange.com/questions/30552/os-x-computer-name-not-matching-what-shows-on-terminal
    
    Goes to great lengths offering possible solutions here, and advice on
    what you should and shouldn't do. Whatever, in my case the "Sharing"
    panel showed the name, but:
    
    ```
    scutil --get ComputerName     # showed the name...
    scutil --get LocalHostName    # showed the name...
    scutil --get HostName         # did NOT show the name...
    ```
    
    `man hostname` says to make hostname changes permanent across reboots
    with `sudo scutil --set Hostname $NAME`, despite the advice in the Stack
    Exchange thread, so I went ahead and did that and everything is working
    now.
    
    In any case, adding some logging so that I'll hopefully notice this if
    it happens again.

--- variables.ts
+++ variables.ts
@@ -1,6 +1,9 @@
 import * as process from 'node:process';
 
 import Context from 'fig/Context.js';
+import {log} from 'fig/console.js';
+
+let hostnameWarningIssued = false;
 
 /**
  * @file
@@ -15,7 +18,18 @@ const variables = {
    * "handle" (eg. "huertas", "retiro").
    */
   get hostHandle() {
-    const handle = Context.attributes.hostname.toLowerCase().split(/\./)[0];
+    const hostname = Context.attributes.hostname;
+    const handle = hostname.toLowerCase().split(/\./)[0];
+
+    if (/^\d+\.\d+\.\d+.\d+$/.test(hostname) && !hostnameWarningIssued) {
+      hostnameWarningIssued = true;
+      // Somewhat sketchily, this is an async function but we're just doing
+      // fire-and-forget on it because: (a) This should be rare; (b) I don't
+      // want to make variable look-up async too.
+      log.warn(
+        `Bad hostname ${hostname} (numeric) produces invalid hostHandle "${handle}"`
+      );
+    }
 
     // Kludgy special-case for Codespaces so that we can cheatingly use per-host
     // config files without knowing the exact host name ahead of time.
