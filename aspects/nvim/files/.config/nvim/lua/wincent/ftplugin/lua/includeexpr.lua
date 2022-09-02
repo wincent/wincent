63bbbdf919fc2cbd90ceb775303a3ea40c6b38ce
Greg Hurrell <greg@hurrell.net>
2022-09-02 19:25:52 +0200

    refactor(nvim): use more descriptive names

--- aspects/nvim/files/.config/nvim/lua/wincent/ftplugin/lua/includeexpr.lua
+++ aspects/nvim/files/.config/nvim/lua/wincent/ftplugin/lua/includeexpr.lua
@@ -4,9 +4,8 @@ local fmt = string.format
 -- See: http://www.lua.org/manual/5.2/manual.html#pdf-package.config
 local separator = string.match(package.config, '^[^\n]')
 
--- Search for lua traditional include paths.
--- This mimics how require internally works.
-local function include_paths(fname)
+-- Search in Lua `package.path` locations.
+local search_package_path = function (fname)
   local paths = string.gsub(package.path, "%?", fname)
   for path in string.gmatch(paths, '[^%;]+') do
     if vim.fn.filereadable(path) == 1 then
@@ -15,8 +14,8 @@ local function include_paths(fname)
   end
 end
 
--- Search for nvim lua include paths
-local function include_rtpaths(fname, ext)
+-- Search in nvim 'runtimepath' directories.
+local search_runtimepath = function(fname, ext)
   local candidate
   for _, path in ipairs(vim.api.nvim_list_runtime_paths()) do
     -- Look for "lua/*.lua".
@@ -33,25 +32,25 @@ local function include_rtpaths(fname, ext)
 end
 
 -- Global function that searches the path for the required file
-local function find_required_path(module)
+local includeexpr = function (module)
   -- Properly change '.' to separator (probably '/' on *nix and '\' on Windows)
   local fname = vim.fn.substitute(module, '\\.', separator, 'g')
   local f
   ---- First search for lua modules
-  f = include_paths(fname)
+  f = search_package_path(fname)
   if f then
     return f
   end
   -- This part is just for nvim modules
-  f = include_rtpaths(fname, "lua")
+  f = search_runtimepath(fname, "lua")
   if f then
     return f
   end
   -- This part is just for nvim modules
-  f = include_rtpaths(fname, "fnl")
+  f = search_runtimepath(fname, "fnl")
   if f then
     return f
   end
 end
 
-return find_required_path
+return includeexpr
