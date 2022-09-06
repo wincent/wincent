ad15076f80468b8a944216a0c7c0466f968a0a95
Greg Hurrell <wincent@github.com>
2022-09-06 19:02:12 +0200

    refactor(nvim): remove unnecessary `{ remap = true }`
    Unnecessary, because this is the default.

--- aspects/nvim/files/.config/nvim/after/plugin/command-t.lua
+++ aspects/nvim/files/.config/nvim/after/plugin/command-t.lua
@@ -43,15 +43,15 @@ vim.g.CommandTWindowFilter = '!&buflisted && &buftype == "nofile" && &filetype !
 -- Lua version doesn't come with any bindings of its own, so we do need to set
 -- these up.
 
-vim.keymap.set('n', '<Leader>b', '<Plug>(CommandTBuffer)', { remap = true })
-vim.keymap.set('n', '<Leader>h', '<Plug>(CommandTHelp)', { remap = true })
-vim.keymap.set('n', '<Leader>t', '<Plug>(CommandTRipgrep)', { remap = true })
+vim.keymap.set('n', '<Leader>b', '<Plug>(CommandTBuffer)')
+vim.keymap.set('n', '<Leader>h', '<Plug>(CommandTHelp)')
+vim.keymap.set('n', '<Leader>t', '<Plug>(CommandTRipgrep)')
 
 -- Note: These ones come from the Ruby version, for now.
 
-vim.keymap.set('n', '<LocalLeader>c', '<Plug>(CommandTCommand)', { remap = true })
-vim.keymap.set('n', '<LocalLeader>h', '<Plug>(CommandTHistory)', { remap = true })
-vim.keymap.set('n', '<LocalLeader>l', '<Plug>(CommandTLine)', { remap = true })
+vim.keymap.set('n', '<LocalLeader>c', '<Plug>(CommandTCommand)')
+vim.keymap.set('n', '<LocalLeader>h', '<Plug>(CommandTHistory)')
+vim.keymap.set('n', '<LocalLeader>l', '<Plug>(CommandTLine)')
 
 -- Convenience for starting Command-T at launch without causing freak-out inside
 -- tmux.
