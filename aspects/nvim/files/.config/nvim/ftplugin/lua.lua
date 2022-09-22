807aed991824253ba841063a4a9585cea69c1296
Greg Hurrell <greg@hurrell.net>
2022-09-22 14:14:47 +0200

    chore: use latest version (v0.15.0) of stylua
    - https://github.com/JohnnyMorganz/StyLua/releases/tag/v0.15.0
    
    This has the `--allow-hidden` switch, which means we can simplify our
    invocation.
    
    However, it did not like this line (using `[=[`/`]=]` delimiters and
    some nested quotations):
    
        vim.opt_local.include = [=[\v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]]=]
    
    Claiming:
    
        error parsing: error occurred while tokenizing: unclosed string at line 2, column 25
    
    The "long bracket" syntax is valid, however, and is described like this:
    
    > Literal strings can also be defined using a long format enclosed by
    > long brackets. We define an opening long bracket of level n as an
    > opening square bracket followed by n equal signs followed by another
    > opening square bracket. So, an opening long bracket of level 0 is
    > written as [[, an opening long bracket of level 1 is written as [=[,
    > and so on. A closing long bracket is defined similarly; for instance,
    > a closing long bracket of level 4 is written as ]====]. A long string
    > starts with an opening long bracket of any level and ends at the first
    > closing long bracket of the same level. Literals in this bracketed
    > form can run for several lines, do not interpret any escape sequences,
    > and ignore long brackets of any other level. They can contain anything
    > except a closing bracket of the proper level.
    
    in the Lua manual:
    
    - https://www.lua.org/manual/5.1/manual.html
    
    To work around this, I just split it over multiple lines and it parses
    fine. I'll report this upstream:
    
    - https://github.com/JohnnyMorganz/StyLua/issues/587

--- aspects/nvim/files/.config/nvim/ftplugin/lua.lua
+++ aspects/nvim/files/.config/nvim/ftplugin/lua.lua
@@ -1,3 +1,7 @@
 -- Set options to open require with gf
-vim.opt_local.include = [=[\v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]]=]
+
+vim.opt_local.include = vim.trim([[
+  \v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]
+]])
+
 vim.opt_local.includeexpr = 'v:lua.wincent.ftplugin.lua.includeexpr(v:fname)'
