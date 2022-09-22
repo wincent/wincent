f3c6a5c396093d1bd72a991d9320814185efa54f
Greg Hurrell <greg@hurrell.net>
2022-09-22 20:36:58 +0200

    chore: update stylua to v0.15.1
    - https://github.com/JohnnyMorganz/StyLua/releases/tag/v0.15.1
    
    This fixes the parser regressions in v0.15.0, and means I can put the
    problematic string back the way it was before. See 807aed991824253ba
    ("chore: use latest version (v0.15.0) of stylua") for context.

--- aspects/nvim/files/.config/nvim/ftplugin/lua.lua
+++ aspects/nvim/files/.config/nvim/ftplugin/lua.lua
@@ -1,7 +1,3 @@
 -- Set options to open require with gf
-
-vim.opt_local.include = vim.trim([[
-  \v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]
-]])
-
+vim.opt_local.include = [=[\v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]]=]
 vim.opt_local.includeexpr = 'v:lua.wincent.ftplugin.lua.includeexpr(v:fname)'
