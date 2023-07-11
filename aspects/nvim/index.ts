54ac1fa829871851b5f3b5151fd4019d7efd9b49
Greg Hurrell <wincent@github.com>
2023-07-11 12:16:06 +0200

    chore(nvim): bump Neovim from v0.9.0 to v0.9.1
    Not expecting any problems here based on the release notes:
    
    - https://github.com/neovim/neovim/releases/tag/v0.9.1

--- aspects/nvim/index.ts
+++ aspects/nvim/index.ts
@@ -51,7 +51,7 @@ task('download Neovim appimage', when('debian'), async () => {
   await fetch({
     dest: '/opt/nvim/nvim.appimage',
     encoding: null,
-    url: 'https://github.com/neovim/neovim/releases/download/v0.9.0/nvim.appimage',
+    url: 'https://github.com/neovim/neovim/releases/download/v0.9.1/nvim.appimage',
     sudo: true,
   });
 });
