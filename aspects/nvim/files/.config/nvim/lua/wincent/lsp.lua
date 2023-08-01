a3427e1ed9d5fa5c88b8f2cdb602a4a0d565e4f7
Greg Hurrell <greg@hurrell.net>
2023-08-01 23:25:19 +0200

    style: run `bin/format`

--- aspects/nvim/files/.config/nvim/lua/wincent/lsp.lua
+++ aspects/nvim/files/.config/nvim/lua/wincent/lsp.lua
@@ -74,7 +74,7 @@ lsp.init = function()
           },
           workspace = {
             -- Make the server aware of Neovim runtime files.
-            library = vim.api.nvim_get_runtime_file("", true),
+            library = vim.api.nvim_get_runtime_file('', true),
           },
         },
       },
