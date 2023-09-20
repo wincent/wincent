12f8d74464935c3585283402b394e5f30650af10
Greg Hurrell <wincent@github.com>
2023-09-20 15:09:20 +0200

    feat(dotfiles): improve `lf` previews
    Make image sizing better. I am not sure what changed here, but `$2` is
    definitely width and `$3` is definitely height now. Added an adjustment
    because it seems to overstate the width.
    
    Use `glow` for markdown, and render JSON using `highlight` instead of
    (useless) `file`.

--- aspects/dotfiles/files/.config/lf/pv.sh
+++ aspects/dotfiles/files/.config/lf/pv.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 FILE="$1"
-HEIGHT="$2"
+WIDTH="$2" # Width of preview area; for full width of `lf` UI, see $lf_width.
+HEIGHT="$3" # Height of preview area; for full height of `lf` UI, see $lf_height.
 
-# TODO: figure out how to avoid hard-coding this; (`tput cols` always returns 80)
-WIDTH=80
+# For some reason, $WIDTH seems to overstate the number of available columns by
+# about 10%, so adjust it down to 90% of initial value.
+WIDTH=$((WIDTH / 10 * 9))
+WIDTH=$((WIDTH < 10 ? $2 : WIDTH)) # Too small. Just leave it as it was.
 
 case $(file --brief --mime-type "$FILE") in
   image/*)
     chafa --fill=block --symbols=block --colors=256 --size="$WIDTH"x"$HEIGHT" "$FILE"
     ;;
-  text/*)
-    highlight --force --out-format=ansi -- "$FILE"
+  application/json | text/*)
+    if [[ $FILE == *.md ]]; then
+      # Need `-s dark` to force color outside of interactive shell:
+      # https://github.com/charmbracelet/glow/issues/440
+      glow -s dark --width "$WIDTH" "$FILE"
+    else
+      highlight --force --out-format=ansi -- "$FILE"
+    fi
     ;;
   *)
     file --brief "$FILE"
