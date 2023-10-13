wincent/wincent

commit f5d77ee8595a205a904b842377cb58c31a6ad0f4

fix(neovim): unbreak Up/Down/C-J/C-K in nvim-cmp
Because default mappings got removed in:

- https://github.com/hrsh7th/nvim-cmp/commit/93cf84f7deb2bdb6

Found via breaking changes issue, which I am now subscribed to:

- https://github.com/hrsh7th/nvim-cmp/issues/231

aspects/nvim/files/.config/nvim/after/plugin/nvim-cmp.lua
@@ -139,8 +139,20 @@ if has_cmp then
         end
       end, { 'i', 's' }),
 
-      ['<C-j>'] = cmp.get_config().mapping['<Down>'],
-      ['<C-k>'] = cmp.get_config().mapping['<Up>'],
+      ['<C-j>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end),
+      ['<C-k>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end),
 
       ['<C-y>'] = cmp.mapping(function(fallback)
         if cmp.visible() then

commit aced499911f05ffb6305401399c467fd2b290ec3

fix(nvim): make `<Up>`/`<Down>` work in nvim-cmp menus again
I think this is a regression. Used to work, broken by an nvim-cmp
update, and then I neglected to fix it when I fixed the other breakage
in f5d77ee8595a205a ("fix(neovim): unbreak Up/Down/C-J/C-K in nvim-cmp",
October 8, 2023).

aspects/nvim/files/.config/nvim/after/plugin/nvim-cmp.lua
@@ -117,6 +117,22 @@ if has_cmp then
     vim.api.nvim_feedkeys(rhs(keys), 'nt', true)
   end
 
+  local select_next_item = function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    else
+      fallback()
+    end
+  end
+
+  local select_prev_item = function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    else
+      fallback()
+    end
+  end
+
   cmp.setup({
     mapping = {
       ['<BS>'] = cmp.mapping(function(_fallback)
@@ -139,20 +155,10 @@ if has_cmp then
         end
       end, { 'i', 's' }),
 
-      ['<C-j>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          fallback()
-        end
-      end),
-      ['<C-k>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        else
-          fallback()
-        end
-      end),
+      ['<C-j>'] = cmp.mapping(select_next_item),
+      ['<Down>'] = cmp.mapping(select_next_item),
+      ['<C-k>'] = cmp.mapping(select_prev_item),
+      ['<Up>'] = cmp.mapping(select_prev_item),
 
       ['<C-y>'] = cmp.mapping(function(fallback)
         if cmp.visible() then