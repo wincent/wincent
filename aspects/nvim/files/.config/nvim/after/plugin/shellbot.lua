7c5e5bc4b823bd9acd3325426842df22566d55ed
Greg Hurrell <greg@hurrell.net>
2023-10-20 20:25:25 +0200

    feat(nvim): improve handling of missing SHELLBOT executable
    * aspects/nvim/files/.config/nvim/pack/bundle/opt/shellbot/lua c1c5927...9997bc7 (1):
      > Merge branch 'healthcheck' into wincent

--- aspects/nvim/files/.config/nvim/after/plugin/shellbot.lua
+++ aspects/nvim/files/.config/nvim/after/plugin/shellbot.lua
@@ -1,8 +1,12 @@
 vim.api.nvim_create_user_command('ChatGPT', function ()
   local has_shellbot, shellbot = pcall(require, 'chatgpt')
-  if has_shellbot then
-    shellbot.chatgpt()
-  else
+  if not has_shellbot then
     vim.api.nvim_err_writeln("error: could not require 'chatgpt'; is the submodule initialized?")
+    return
   end
+  if vim.env['SHELLBOT'] == nil or vim.fn.executable(vim.env['SHELLBOT']) ~= 1 then
+    vim.api.nvim_err_writeln('error: SHELLBOT does not appear to be executable')
+    return
+  end
+  shellbot.chatgpt()
 end, {})
