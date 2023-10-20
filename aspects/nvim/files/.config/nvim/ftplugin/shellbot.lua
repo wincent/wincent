f82dad0ea82ceea8c90ade209b68d3780ef0b520
Greg Hurrell <greg@hurrell.net>
2023-10-20 20:47:48 +0200

    refactor(nvim): set a couple of options to improve appearance
    In shellbot buffers.

--- aspects/nvim/files/.config/nvim/ftplugin/shellbot.lua
+++ aspects/nvim/files/.config/nvim/ftplugin/shellbot.lua
@@ -1,3 +1,6 @@
+vim.bo.textwidth = 0
+vim.wo.showbreak = ''
+
 local has_shellbot = require('chatgpt')
 if has_shellbot then
   vim.keymap.set({ 'i', 'n' }, '<M-CR>', ChatGPTSubmit, { buffer = true })
