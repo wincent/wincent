409b27b172c7901efe079c372ade081bfbc62f9f
Greg Hurrell <wincent@github.com>
2023-11-15 12:14:48 +0100

    refactor(nvim): port `wincent#debug#log()` to Lua

--- aspects/nvim/files/.config/nvim/autoload/wincent/debug.vim
+++ aspects/nvim/files/.config/nvim/autoload/wincent/debug.vim
@@ -86,7 +86,3 @@ function! wincent#debug#compiler() abort
   /\v^finish>/+1,$ :cgetbuffer
   copen
 endfunction
-
-function! wincent#debug#log(string) abort
-  call writefile([a:string], '/tmp/wincent-vim-debug.txt', 'aS')
-endfunction
---
+++ aspects/nvim/files/.config/nvim/lua/wincent/debug.lua
@@ -0,0 +1,33 @@
+local debug = nil
+
+local writefile = function(lines)
+  vim.fn.writefile(lines, '/tmp/wincent-nvim-debug.txt', 'aS')
+end
+
+-- TODO: extract this?
+local is_list = function(value)
+  if type(value) ~= 'table' then
+    return false
+  elseif #value > 0 then
+    return true
+  else
+    for _k, _v in pairs(value) do
+      return false
+    end
+  end
+  return true
+end
+
+debug = function(value)
+  if type(value) == 'string' then
+    writefile({ value })
+  elseif is_list(value) then
+    for _, v in pairs(value) do
+      debug(v)
+    end
+  else
+    writefile({ vim.inspect(value) })
+  end
+end
+
+return debug
