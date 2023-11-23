f36a853a961893ef009dffec7eae6c5cfe8120de
Greg Hurrell <greg@hurrell.net>
2023-11-23 22:40:04 +0100

    chore(dotfiles): prepare for new Involuntary Reboot Log
    Because naturally, I need it after 1 day.

--- aspects/dotfiles/files/.zsh/bin/.irl.rb
+++ aspects/dotfiles/files/.zsh/bin/.irl.rb
@@ -4,10 +4,11 @@ require 'date'
 
 # 2017-10-31: Personal MacBook Pro (Haswell, Mid-2015, dual graphics).
 # 2021-05-31: GitHub MacBook Pro (13-Inch "M1" 8-Core 3.2 (2020)).
+# 2023-11-22: Personal MacBook Pro ("M3 Max" 16 CPU/40 GPU 16" (2023)).
 
-year = (ARGV[0] || 2017).to_i
-month = (ARGV[1] || 10).to_i
-day = (ARGV[2] || 31).to_i
+year = (ARGV[0] || 2023).to_i
+month = (ARGV[1] || 11).to_i
+day = (ARGV[2] || 22).to_i
 diff = Date.today - Date.new(year, month, day)
 puts "Days to date: #{diff.to_i}"
 print 'How many failures so far? '
