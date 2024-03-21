3322f9a12fb159b9c46058957eb554d3594a014d
Greg Hurrell <wincent@github.com>
2024-03-21 15:17:46 +0100

    feat(nix): add sample `shell.nix` file and some recipes
    Going through the tutorial, like this page and its neighbors:
    
    - https://nix.dev/tutorials/first-steps/declarative-shell

---
+++ aspects/nix/files/shell.nix
@@ -0,0 +1,11 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    cowsay
+    lolcat
+  ];
+}
