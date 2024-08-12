4c9c30d717751833c9bf19e11558298725e9aedb
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-08-12 11:12:32 +0200

    fix(nvim): try not to add duplicate "tsx" filetype
    Not sure exactly when this is happening, but I have seen some buffers
    getting extra ".tsx" appended to their filetypes. `:verbose set ft?`
    tells me it is being set from Lua, but not from where. Running `:e!`
    appears to set things back to how they should be.
    
    So, without really knowing what is causing it to happen, add a layer of
    defense by conditionally adding ".tsx" only if it isn't already present.

--- aspects/nvim/files/.config/nvim/ftdetect/tsx.lua
+++ aspects/nvim/files/.config/nvim/ftdetect/tsx.lua
@@ -1,1 +1,11 @@
-wincent.vim.autocmd('BufNewFile,BufRead', '*.tsx', 'noautocmd set filetype+=.tsx')
+local has_tsx_filetype = function()
+  return vim.regex('\\v<tsx>'):match_str(vim.o.filetype)
+end
+
+local set_tsx = function()
+  if not has_tsx_filetype() then
+    vim.cmd('noautocmd set filetype+=.tsx')
+  end
+end
+
+wincent.vim.autocmd('BufNewFile,BufRead', '*.tsx', set_tsx)
