e4e0a90217fb6eb1f5c9c50481896f75cff83204
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-08-15 12:47:44 +0200

    feat(nvim): add autocmd to not accidentally kill last shellbot window
    I routinely hit `<space>w` or `<space>q` and end up losing the last
    window and the state it contains. This makes it a little bit harder to
    do so unintentionally.

--- aspects/nvim/files/.config/nvim/after/plugin/shellbot.lua
+++ aspects/nvim/files/.config/nvim/after/plugin/shellbot.lua
@@ -14,3 +14,27 @@ vim.api.nvim_create_user_command('ChatGPT', function()
   end
   vim.api.nvim_err_writeln('error: SHELLBOT does not appear to be executable')
 end, {})
+
+-- Set up an autocmd to stop me from accidentally quitting vim when shellbot is
+-- the only thing running in it. I do this all the time, losing valuable state.
+vim.api.nvim_create_autocmd('QuitPre', {
+  pattern = '*',
+  callback = function ()
+    local buftype = vim.bo.buftype
+    local filetype = vim.bo.filetype
+    local win_count = #vim.api.nvim_tabpage_list_wins(0)
+
+    if filetype == 'shellbot' and buftype == 'nofile' and win_count == 1 then
+      vim.api.nvim_err_writeln('')
+      vim.api.nvim_err_writeln('Use :q! if you really want to quit the last shellbot window')
+      vim.api.nvim_err_writeln('')
+
+      -- Will make Neovim abort the quit with:
+      --
+      --    E37: No write since last change
+      --    E162: No write since last change for buffer "[No Name]"
+      --
+      vim.bo.buftype = ''
+    end
+  end,
+})
