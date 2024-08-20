740bd74d5c450bc985f3b5d0b1716a92892eaa19
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-08-20 13:12:18 +0200

    fix(violentmonkey): update GitHub scripts to account for DOM changes
    On the one hand, finding the comment label is a bit easier now.
    
    On the other, needed to update our targeting to find comment inputs.

--- aspects/violentmonkey/templates/UserScripts/github/makeTextAreasBigger.user.js
+++ aspects/violentmonkey/templates/UserScripts/github/makeTextAreasBigger.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Make GitHub PR textareas bigger
 // @namespace    https://wincent.dev/
-// @version      0.7
+// @version      0.8
 // @description  Make GitHub PR textareas bigger
 // @author       Greg Hurrell <greg@hurrell.net>
 // @match        https://github.com/*
@@ -16,9 +16,8 @@
   const style = document.createElement('style');
 
   style.innerHTML = `
-        .previewable-comment-form textarea {
-            min-height: 300px; /* normally 100px, which is ridiculous */
-            max-height: none; /* normally 500px */
+        .CommentBox-input {
+            min-height: 300px; /* normally 102px, which is ridiculous */
         }
     `;
 
