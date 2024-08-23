000736d2ccc6de07ca71ed24a2600525863f37b3
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-08-23 18:57:50 +0200

    refactor(dotfiles): change border widths
    As the tmux splits are somehow more fundamental than the Neovim ones,
    make them heavier.

--- aspects/nvim/files/.config/nvim/init.lua
+++ aspects/nvim/files/.config/nvim/init.lua
@@ -34,7 +34,7 @@ vim.opt.fillchars = {
   diff = '∙', -- BULLET OPERATOR (U+2219, UTF-8: E2 88 99)
   eob = ' ', -- NO-BREAK SPACE (U+00A0, UTF-8: C2 A0) to suppress ~ at EndOfBuffer
   fold = '·', -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
-  vert = '┃', -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
+  vert = '│', -- BOX DRAWINGS LIGHT VERTICAL (U+2502, UTF-8: E2 94 82)
 }
 vim.opt.foldlevelstart = 99 -- start unfolded
 vim.opt.foldmethod = 'indent' -- not as cool as syntax, but faster
