9f8caf38c7a28c71f788f1eae64ba968f1dff44e
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-10-10 22:11:04 +0200

    chore(dotfiles): add some Yazi customizations
    - show owner:group in status bar
    - show symlink targets in status bar
    - show "→" instead of "->" for symlinks

--- aspects/dotfiles/files/.config/yazi/init.lua
+++ aspects/dotfiles/files/.config/yazi/init.lua
@@ -1,2 +1,61 @@
+--
 -- Plug-ins.
+--
+
 require('full-border'):setup()
+
+--
+-- Additions
+--
+
+-- Show owner:group in status bar.
+--
+-- See: https://yazi-rs.github.io/docs/tips#user-group-in-status
+Status:children_add(function()
+  local h = cx.active.current.hovered
+  if h == nil or ya.target_family() ~= 'unix' then
+    return ui.Line {}
+  end
+
+  return ui.Line {
+    ui.Span(' '),
+    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg('magenta'),
+    ui.Span(':'),
+    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg('magenta'),
+    ui.Span(' '),
+  }
+end, 500, Status.RIGHT)
+
+--
+-- Overrides.
+--
+
+-- Make `Entity:symlink()` render "→" instead of "->".
+--
+-- Original: https://github.com/sxyazi/yazi/blob/7c445cef1fd9f/yazi-plugin/preset/components/entity.lua#L70-L77
+function Entity:symlink()
+  if not MANAGER.show_symlink then
+    return ui.Line {}
+  end
+
+  local to = self._file.link_to
+  return to and ui.Line(' → ' .. tostring(to)):italic() or ui.Line {}
+end
+
+-- Make status bar show symlink target.
+--
+-- See: https://yazi-rs.github.io/docs/tips#symlink-in-status
+--
+-- Original: https://github.com/sxyazi/yazi/blob/7c445cef1fd9f/yazi-plugin/preset/components/status.lua#L60-L67
+function Status:name()
+  local h = self._tab.current.hovered
+  if not h then
+    return ui.Line {}
+  end
+
+  local linked = ''
+  if h.link_to ~= nil then
+    linked = ' → ' .. tostring(h.link_to)
+  end
+  return ui.Line(' ' .. h.name .. linked)
+end
