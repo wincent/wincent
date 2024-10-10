ff2df33bbed5cd4619428fef0b3a5b3fdb5ddd4f
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-10-10 22:41:43 +0200

    feat(dotfiles): set up "smart enter" for Yazi

---
+++ aspects/dotfiles/files/.config/yazi/plugins/smart-enter.yazi/init.lua
@@ -0,0 +1,7 @@
+-- https://yazi-rs.github.io/docs/tips#smart-enter
+return {
+  entry = function()
+    local h = cx.active.current.hovered
+    ya.manager_emit(h and h.cha.is_dir and 'enter' or 'open', { hovered = true })
+  end,
+}
