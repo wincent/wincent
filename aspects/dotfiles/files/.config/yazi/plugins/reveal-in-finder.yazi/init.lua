2dcdff16ef65cb5c9b4e073e643d093b9d82be34
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-10-11 13:07:05 +0200

    feat(dotfiles): add "s" keymap to show in Finder
    Files get revealed (ie. we open their containing folder) and folders get
    opened.

---
+++ aspects/dotfiles/files/.config/yazi/plugins/reveal-in-finder.yazi/init.lua
@@ -0,0 +1,13 @@
+return {
+  entry = function()
+    local h = cx.active.current.hovered
+    if h then
+      local quoted_path = ya.quote(tostring(h.url))
+      if h.cha.is_dir then
+        ya.manager_emit('shell', { 'open ' .. quoted_path, confirm = true })
+      elseif h then
+        ya.manager_emit('shell', { 'open -R ' .. quoted_path, confirm = true })
+      end
+    end
+  end,
+}
