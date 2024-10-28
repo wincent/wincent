78d13e297d87e80a163e8e930ed4377d9a4b2b76
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-10-28 12:06:55 +0100

    refactor(nvim): avoid deprecation warnings
    As reported by `:checkhealth vim.deprecated`, `vim.fn.sign_define` is
    deprecated and you're supposed to use `vim.diagnostic.config` instead.

--- aspects/nvim/files/.config/nvim/after/plugin/nvim-lspconfig.lua
+++ aspects/nvim/files/.config/nvim/after/plugin/nvim-lspconfig.lua
@@ -1,13 +1,28 @@
 wincent.lsp.init()
 
--- Based on: https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
-vim.fn.sign_define(
-  'DiagnosticSignError',
-  { text = '✖', texthl = 'DiagnosticSignError', numhl = 'DiagnosticSignError' }
-)
-vim.fn.sign_define('DiagnosticSignHint', { text = '➤', texthl = 'DiagnosticSignHint', numhl = 'DiagnosticSignHint' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = 'ℹ', texthl = 'DiagnosticSignInfo', numhl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '⚠', texthl = 'DiagnosticSignWarn', numhl = 'DiagnosticSignWarn' })
+-- See also: https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '✖',
+      [vim.diagnostic.severity.HINT] = '➤',
+      [vim.diagnostic.severity.INFO] = 'ℹ',
+      [vim.diagnostic.severity.WARN] = '⚠',
+    },
+    texthl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+    },
+  },
+})
 
 wincent.vim.augroup('WincentLanguageClientAutocmds', function()
   wincent.vim.autocmd('ColorScheme', '*', wincent.lsp.set_up_highlights)
