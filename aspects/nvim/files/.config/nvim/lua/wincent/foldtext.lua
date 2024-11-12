dc6e3bef3990efea8e17e142bd9c54f4e81153c4
Greg Hurrell <greg.hurrell@datadoghq.com>
2024-11-12 10:04:52 +0100

    refactor(nvim): improve foldtext implementation
    To better maintain indent. The `folddashes` variable isn't precisely
    indent aware like we can be in this function, so we do a bit more work
    to make the indented text line up with where it was before folding, if
    possible.

--- aspects/nvim/files/.config/nvim/lua/wincent/foldtext.lua
+++ aspects/nvim/files/.config/nvim/lua/wincent/foldtext.lua
@@ -11,10 +11,22 @@ local small_l = 'ℓ'
 --   »··[2ℓ]··: source $HOME/.config/nvim/pack/bundle/opt/vim-pathogen/autoload/pathogen.vim···································
 --
 local foldtext = function()
-  local lines = '[' .. (vim.v.foldend - vim.v.foldstart + 1) .. small_l .. ']'
-  local first = ({ vim.api.nvim_buf_get_lines(0, vim.v.foldstart - 1, vim.v.foldstart, true)[1]:gsub(' *', '', 1) })[1]
-  local dashes = ({ vim.v.folddashes:gsub('-', middot) })[1]
-  return raquo .. middot .. middot .. lines .. dashes .. ': ' .. first
+  local line_count = vim.v.foldend - vim.v.foldstart + 1
+  local lines = '[' .. line_count .. small_l .. ']'
+  local first = vim.api.nvim_buf_get_lines(0, vim.v.foldstart - 1, vim.v.foldstart, true)[1]
+  local tabs = first:match('^%s*'):gsub(' +', ''):len()
+  local spaces = first:match('^%s*'):gsub('\t', ''):len()
+  local indent = spaces + tabs * vim.bo.tabstop
+  local stripped = first:match('^%s*(.-)$')
+  local prefix = raquo .. middot .. middot .. lines
+  local suffix = ': '
+
+  -- Can't usefully use string.len() on UTF-8.
+  local prefix_len = tostring(line_count):len() + 6
+
+  local dash_count = math.max(indent - prefix_len - string.len(suffix), 0)
+  local dashes = string.rep(middot, dash_count)
+  return prefix .. dashes .. suffix .. stripped
 end
 
 return foldtext
