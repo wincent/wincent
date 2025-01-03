b08f05748a1c3a1401e1a7c24a91dd98c105c2dd
Greg Hurrell <greg@hurrell.net>
2025-01-04 00:22:47 +0100

    fix(nvim): handle other kinds of LSP input
    Feature in the parent commit was working in TS files because the TS
    language server gives us a `completion_item.textEdit`. Noticed in Lua
    that I'm only getting `completion_item.label`.
    
    So, implement a fallback.

--- aspects/nvim/files/.config/nvim/after/plugin/nvim-cmp.lua
+++ aspects/nvim/files/.config/nvim/after/plugin/nvim-cmp.lua
@@ -176,17 +176,24 @@ if has_cmp then
   -- `cmp.ConfirmBehavior.Replace`:
   local confirm = function(entry)
     local behavior = cmp.ConfirmBehavior.Replace
-    if entry and entry.completion_item.textEdit then
+    if entry then
+      local completion_item = entry.completion_item
+      local newText = ''
+      if completion_item.textEdit then
+        newText = completion_item.textEdit.newText
+      elseif type(completion_item.insertText) == 'string' and completion_item.insertText ~= '' then
+        newText = completion_item.insertText
+      else
+        newText = completion_item.word or completion_item.label or ''
+      end
+
       -- How many characters will be different after the cursor position if we
       -- replace?
       local diff_after = math.max(0, entry.replace_range['end'].character + 1) - entry.context.cursor.col
 
       -- Does the text that will be replaced after the cursor match the suffix
       -- of the `newText` to be inserted? If not, we should `Insert` instead.
-      if
-        entry.context.cursor_after_line:sub(1, diff_after)
-        ~= entry.completion_item.textEdit.newText:sub(-diff_after)
-      then
+      if entry.context.cursor_after_line:sub(1, diff_after) ~= newText:sub(-diff_after) then
         behavior = cmp.ConfirmBehavior.Insert
       end
     end
