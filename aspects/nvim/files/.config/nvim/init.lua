4a33dbef592e71b1b9d5180b441bde992d5ae357
Greg Hurrell <greg@hurrell.net>
2025-01-18 14:11:29 +0100

    chore(nvim): set `foldminlines` to 0
    As help describes:
    
                                                   *'foldminlines'* *'fml'*
        'foldminlines' 'fml'  number  (default 1)
                                local to window
                Sets the number of screen lines above which a fold can be
                displayed closed. Also for manually closed folds. With the
                default value of one a fold can only be closed if it takes
                up two or more screen lines. Set to zero to be able to close
                folds of just one screen line. Note that this only has an
                effect on what is displayed. After using "zc" to close a
                fold, which is displayed open because it's smaller than
                'foldminlines', a following "zc" may close a containing
                fold.
    
    This was puzzling me the other day: that _some_ one-line blocks were
    foldable if they exceeded 1 "screen line" (or could exceed it, if `:set
    wrap` were in effect). The inconsistency bugs me, so lets get rid of it.

--- aspects/nvim/files/.config/nvim/init.lua
+++ aspects/nvim/files/.config/nvim/init.lua
@@ -38,6 +38,7 @@ vim.opt.fillchars = {
 }
 vim.opt.foldlevelstart = 99 -- start unfolded
 vim.opt.foldmethod = 'expr'
+vim.opt.foldminlines = 0 -- Allow closing even 1-line folds.
 vim.opt.foldexpr = 'v:lua.wincent.foldexpr(v:lnum)'
 vim.opt.foldtext = 'v:lua.wincent.foldtext()'
 vim.opt.formatoptions = vim.opt.formatoptions + 'j' -- remove comment leader when joining comment lines
