fee810b2be44c0ff399e5058250f989c67068008
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-01-21 13:30:23 +0100

    refactor(nvim): avoid unnecessary "forward declaration"

--- aspects/nvim/files/.config/nvim/lua/wincent/debug.lua
+++ aspects/nvim/files/.config/nvim/lua/wincent/debug.lua
@@ -1,24 +1,24 @@
-local debug = nil
-
 local writefile = function(lines)
   vim.fn.writefile(lines, '/tmp/wincent-nvim-debug.txt', 'as')
 end
 
--- TODO: extract this?
+-- TODO: extract this? it's the same as wincent.commandt.private.is_list()
 local is_list = function(value)
   if type(value) ~= 'table' then
     return false
   elseif #value > 0 then
     return true
   else
+    --- @diagnostic disable-next-line: unused-local
     for _k, _v in pairs(value) do
       return false
     end
   end
+  --- @diagnostic disable-next-line: unreachable-code
   return true
 end
 
-debug = function(value)
+local function debug(value)
   if type(value) == 'string' then
     writefile({ value })
   elseif is_list(value) then
