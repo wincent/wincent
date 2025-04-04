3b44bf52a36c2c2c856f3690a49f2a136ba58f87
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-04-04 12:44:57 +0200

    refactor(nvim): replace `setlocal()` in `spell()` function
    One more step. See parent commit for more context.

--- aspects/nvim/files/.config/nvim/lua/wincent/vim/spell.lua
+++ aspects/nvim/files/.config/nvim/lua/wincent/vim/spell.lua
@@ -1,10 +1,8 @@
-local setlocal = wincent.vim.setlocal
-
 -- Turn on spell-checking.
-local spell = function()
-  setlocal('spell')
-  setlocal('spellfile', vim.fn.expand('~/.config/nvim/spell/en.utf-8.add'))
-  setlocal('spelllang', 'en,es')
+local function spell()
+  vim.opt_local.spell = true
+  vim.opt_local.spellfile = vim.fn.expand('~/.config/nvim/spell/en.utf-8.add')
+  vim.opt_local.spelllang = 'en,es'
 end
 
 return spell
