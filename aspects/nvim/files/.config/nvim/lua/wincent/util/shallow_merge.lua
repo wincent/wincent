b1e67acb2b3448249b85ba128769d0906fbe0d94
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-07-04 11:11:04 +0200

    chore(nvim): remove some dead code
    Last reference to `shallow_merge()` went away in fdb20156e1096980
    ("refactor(nvim): switch to new keymap API provided by Neovim v0.7.0",
    2022-04-29).
    
    Last reference to `get_key_for_fn()` went away in 9e8550d10cb87ff9
    ("refactor(nvim): turn `wincent.vim.autocmd()` into a thinner wrapper",
    2025-06-11).

--- aspects/nvim/files/.config/nvim/lua/wincent/util/shallow_merge.lua
+++
@@ -1,7 +0,0 @@
--- Shallow table merge, merges `source` into `dest`, returning a copy (the
--- original tables are not modified).
-local shallow_merge = function(dest, source)
-  return vim.tbl_extend('force', dest, source)
-end
-
-return shallow_merge
