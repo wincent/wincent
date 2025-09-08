00a668487b60b23fc7ab107df0aa80ec4da5f37c
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-09-08 13:44:45 +0200

    refactor(nvim): remove workaround for folding bug
    Issue:
    
    - https://github.com/neovim/neovim/issues/33926
    
    has now been fixed, by:
    
    - https://github.com/neovim/neovim/pull/35396
    
    which itself was backported to v0.11.4:
    
    - https://github.com/neovim/neovim/commit/3cf9dac2bacaa3c5044d5e622184010a6b18e97b
    
    which came out last week:
    
    - https://github.com/neovim/neovim/releases/tag/v0.11.4
    
    I confirmed that the original repro recipe from the issue no longer
    triggers the bug.

--- aspects/nvim/files/.config/nvim/ftplugin/markdown.lua
+++ aspects/nvim/files/.config/nvim/ftplugin/markdown.lua
@@ -8,7 +8,4 @@ local has_treesitter = pcall(require, 'nvim-treesitter')
 if has_treesitter then
   vim.opt_local.foldmethod = 'expr'
   vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-
-  -- Workaround for: https://github.com/neovim/neovim/issues/33926
-  vim.opt_local.foldminlines = 1
 end
