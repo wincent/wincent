ec85a2fb6b445272cc59b3b2a5be1e675de1752d
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-09-18 11:00:56 +0200

    fix(violentmonkey): attempt to fix broken "Autoclose Appgate" script
    It's not working, although I can see the script is active and the
    `document.body.textContent` definitely matches. Let's see if this
    metadata tweak allows it to actually work.

--- aspects/violentmonkey/templates/UserScripts/appgate/autoclose.user.js
+++ aspects/violentmonkey/templates/UserScripts/appgate/autoclose.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Autoclose Appgate
 // @namespace    https://wincent.dev/
-// @version      0.4
+// @version      0.5
 // @description  Autoclose the Appgate success window
 // @author       Greg Hurrell <greg@hurrell.net>
 // @match        http://127.0.0.1:29001/saml
 // @grant        window.close
-// @run-at       document-start
+// @run-at       document-end
 // @downloadURL  http://localhost/~<%= variables.username %>/UserScripts/appgate/autoclose.user.js
 // @updateURL    http://localhost/~<%= variables.username %>/UserScripts/appgate/autoclose.user.js
 // ==/UserScript==
