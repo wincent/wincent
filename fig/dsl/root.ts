3703b9d81ce9d5cacaea0145339ad319a394c166
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-11-24 11:17:58 +0100

    refactor(fig): simplify `root.ts`
    As noted in the comment, since skipping the build step and taking
    advantage of Node.js's newfound ability to strip type annotations (see
    332bbb394224f7c81, "refactor(fig): run TS source directly with Node",
    2025-06-06), we no longer run out of `lib/`, so the determination of the
    repo root can be massively simplified.

--- fig/dsl/root.ts
+++ fig/dsl/root.ts
@@ -1,33 +1,11 @@
 import {dirname, join} from 'node:path';
 import {fileURLToPath} from 'node:url';
 
-import {existsSync} from '../fs.ts';
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /**
- * Determine repo root directory by walking up directory tree until we see the
- * "package-lock.json". We used to do this dynamically instead of using a hardcoded relative
- * path because the root could be in a different position depending on whether you were
- * starting from the "fig/dsl/root.ts" (source) or from "lib/fig/dsl/root.js"
- * (compiled).
- *
- * TODO: Now that we are running the source files directly, revisit this 👆
+ * The repo root directory.
  */
-const root = (function find(path): string {
-  const target = 'package-lock.json';
-
-  if (existsSync(join(path, target))) {
-    return path;
-  } else {
-    const next = dirname(path);
-
-    if (next === path) {
-      throw new Error(`Searched up to ${path} without finding ${target}`);
-    }
-
-    return find(dirname(path));
-  }
-})(__dirname);
+const root = join(__dirname, '../..');
 
 export default root;
