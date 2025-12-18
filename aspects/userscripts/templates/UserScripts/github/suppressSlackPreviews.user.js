99e4fd42ba1d9ed624f4afeb9cc19cfffc068b15
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-12-18 11:46:47 +0100

    feat(userscripts): add script to suppress PR previews in Slack
    Based on a similar script that was shared at work, but instead of
    just running every second, additionally runs eagerly on page load.
    
    Appending "/s" to the URL stops Slack from showing a preview, but
    doesn't break the URL (ie. GitHub dutifully serves up the PR page and,
    critically, it doesn't redirect you back to the canonical URL without
    the suffix).
    
    I tried to find the origin of the "append /s" idea on the web, but my
    search fu is failing me. For some other suffixes that I tried, I get
    redirected straight back to the canonical URL. For some reason "/s"
    sticks.
    
    Change-Id: wrmrkqnkuozrukurvpsvwxoptwmsoqqn

---
+++ aspects/userscripts/templates/UserScripts/github/suppressSlackPreviews.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Suppress Slack previews for GitHub URLs
+// @namespace    https://wincent.dev/
+// @version      0.3
+// @description  Stops Slack from showing huge previews for GitHub URLs by appending an "/s" to the end
+// @author       Greg Hurrell <greg@hurrell.net>
+// @match        https://github.com/*
+// @grant        none
+// @downloadURL  http://localhost/~<%= variables.username %>/UserScripts/github/suppressSlackPreviews.user.js
+// @updateURL    http://localhost/~<%= variables.username %>/UserScripts/github/suppressSlackPreviews.user.js
+// ==/UserScript==
+
+(function () {
+  function suppress() {
+      if (/^\/[-\w]+\/[-\w]+\/pull\/\d+$/.test(location.pathname)) {
+          const url = new URL(window.location);
+          url.pathname += '/s';
+          history.replaceState(null, undefined, url.toString());
+      }
+  }
+
+  // Run on initial load...
+  suppress();
+
+  // ...and every 1s after that.
+  setInterval(suppress, 1000);
+})();
+
+// <%= variables.figManaged %>
