7032429458ac1917b4ebbcde416b6c69f038050f
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-12-31 09:04:49 +0100

    chore(backup): don't create `~/Backups` on work machine
    As I don't use it.
    
    Change-Id: qvmovnlmzwuxptsvpytqqmqwqwtnnuvv

--- aspects/backup/index.ts
+++ aspects/backup/index.ts
@@ -2,6 +2,6 @@ import {file, helpers, task} from 'fig';
 
 const {when} = helpers;
 
-task('create ~/Backups', when('wincent'), async () => {
+task('create ~/Backups', when('wincent', 'personal'), async () => {
   await file({path: '~/Backups', state: 'directory'});
 });
