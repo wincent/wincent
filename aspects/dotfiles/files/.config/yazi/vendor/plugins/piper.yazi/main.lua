5121e8ebd138f28e294cdda456962792e4d34c57
Greg Hurrell <greg.hurrell@datadoghq.com>
2026-07-02 13:44:42 +0200

    chore(nvim): run `bin/update-dependencies`
    Neovim v0.12.4 just came out:
    
    - https://github.com/neovim/neovim/commit/68ea43cd0c28af25cd47731308c94fedfcfd1b0b
    
    and I wanted to see if any plugins required or had updates (the answer:
    not really):
    
    ================================================================================
    CHANGELOG
    ================================================================================
    
    * aspects/dotfiles/files/.config/yazi/vendor/plugins 5d461d8..8cd50c6 (1):
    
      8cd50c6 feat: support nightly file icon API
    
    * aspects/nvim/files/.config/nvim/pack/bundle/opt/nvim-lspconfig 292f444..d224a19 (3):
    
      d224a192 docs: update generated annotations skip-checks: true
      b28e0c2f docs: update configs.md skip-checks: true
      caca08c8 docs(angularls): update link to angularls #4466
    
    ================================================================================
    2 dependencies updated
    ================================================================================
    
    Change-Id: oyzulyxrmssrskvypyxnsyummtsroxrv

--- aspects/dotfiles/files/.config/yazi/vendor/plugins/piper.yazi/main.lua
+++ aspects/dotfiles/files/.config/yazi/vendor/plugins/piper.yazi/main.lua
@@ -57,10 +57,18 @@ function M.format(job, lines)
 	for i = 1, #lines do
 		lines[i] = lines[i]:gsub("[\r\n]+$", "")
 
-		local icon = File({
-			url = Url(lines[i]),
-			cha = Cha { mode = tonumber(lines[i]:sub(-1) == "/" and "40700" or "100644", 8) },
-		}):icon()
+		local icon
+		if th.icon then
+			icon = th.icon:match(File {
+				url = Url(lines[i]),
+				cha = Cha { mode = tonumber(lines[i]:sub(-1) == "/" and "40700" or "100644", 8) },
+			})
+		else -- TODO: remove
+			icon = File({
+				url = Url(lines[i]),
+				cha = Cha { mode = tonumber(lines[i]:sub(-1) == "/" and "40700" or "100644", 8) },
+			}):icon()
+		end
 
 		if icon then
 			lines[i] = ui.Line { ui.Span(" " .. icon.text .. " "):style(icon.style), lines[i] }
