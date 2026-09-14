a6e215a0cd06f47e6d2e0a9d4e6d7cb572c0d207
Greg Hurrell <greg.hurrell@datadoghq.com>
2026-09-14 18:48:03 +0200

    feat(nvim): detect Zsh startup files better
    Note the TODO, which is about adding an explicit ".zsh" extension where
    possible to make detection easier (automatic even).
    
    Change-Id: xmwzyomtmwxxmxukxovkwxpxytoqroox

---
+++ aspects/nvim/files/.config/nvim/ftdetect/zsh.lua
@@ -0,0 +1,20 @@
+local autocmd = require('wincent.nvim.autocmd')
+
+-- TODO: rename files under ~/.zsh to use ".zsh" extensions, to obviate the need
+-- for some of these patterns (not the autoloaded functions, because they have
+-- to be extensionless).
+vim.filetype.add({
+  pattern = {
+    -- Anything without a dot.
+    ['.*/%.zsh/[^./]+'] = 'zsh',
+    -- Anything without a dot, plus a ".private" extension.
+    ['.*/%.zsh/[^/]+%.private'] = 'zsh',
+    -- Anything without a dot under "functions.d/".
+    ['.*/%.zsh/functions%.d/[^./]+'] = 'zsh',
+    -- Anything without a dot under "host/".
+    ['.*/%.zsh/host/[^./]+'] = 'zsh',
+  }
+})
+
+-- Override built-in ftdetect which classifies this as "exports" (NFS) filetype.
+autocmd('BufNewFile,BufRead', '*/.zsh/exports', 'set filetype=zsh')
